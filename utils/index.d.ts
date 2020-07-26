58ec4ecf79324c20498f4a5e2434421fc48a0516
antfu <anthonyfu117@hotmail.com>
2020-07-26 10:25:06 +0800

    feat(utils): add IsAny

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -1,4 +1,5 @@
-type NOT_EQUAL_INTERNAL<VALUE, EXPECTED> = UnionToIntersection<VALUE> extends UnionToIntersection<EXPECTED>
+// @internal
+export type NOT_EQUAL_INTERNAL<VALUE, EXPECTED> = UnionToIntersection<VALUE> extends UnionToIntersection<EXPECTED>
   ? UnionToIntersection<EXPECTED> extends UnionToIntersection<VALUE>
     ? false
     : true
@@ -10,6 +11,9 @@ export type ExpectFalse<T extends false> = T
 
 export type NotEqual<VALUE, EXPECTED> = true extends NOT_EQUAL_INTERNAL<VALUE, EXPECTED> ? true : false
 export type Equal<VALUE extends EXPECTED, EXPECTED> = NotEqual<VALUE, EXPECTED> extends false ? true : false
+
+// https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+export type IsAny<T> = 0 extends (1 & T) ? true : false
 
 export type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false
 export type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> = ARGS extends Parameters<FUNC>
