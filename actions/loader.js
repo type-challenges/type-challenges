68e623cb26b4f9e3455ec61a54cfc11e8fc7b163
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-04 16:04:11 +0800

    chore: fix

--- actions/loader.js
+++ actions/loader.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-global-assign */
-const r = require('esm')(module)
-r('esbuild-register/register')
+require = require('esm')(module)
+require('esbuild-register/register')
 
 module.exports = (path, github, context, core) => {
   return require(path)(github, context, core)
