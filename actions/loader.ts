3e083a9f66f5da33c7bd7ca847dc38a3f0d008b2
sinoon <sinoon1218@gmail.com>
2020-08-04 17:54:38 +0800

    chore(debug): debug info

---
+++ actions/loader.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import { context, getOctokit } from '@actions/github'
+import * as io from '@actions/io'
+
+process.on('unhandledRejection', handleError)
+main().catch(handleError)
+
+async function main(): Promise<void> {
+  const token = process.argv[2]
+  const fnName = process.argv[3]
+  const github = getOctokit(token)
+
+  const fn = await import(`./${fnName}.ts`)
+  fn.default(github, context, core, io)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function handleError(err: any): void {
+  console.error(err)
+  core.setFailed(`Unhandled error: ${err}`)
+}
