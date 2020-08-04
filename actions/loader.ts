f09d491d57c094ae27e90233a7c17cbb09a8466f
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-04 16:22:22 +0800

    chore: fix

--- actions/loader.ts
+++ actions/loader.ts
@@ -5,28 +5,14 @@ import * as io from '@actions/io'
 process.on('unhandledRejection', handleError)
 main().catch(handleError)
 
-type Options = {
-  log?: Console
-  userAgent?: string
-  previews?: string[]
-}
-
 async function main(): Promise<void> {
-  const token = core.getInput('github-token', { required: true })
-  const debug = core.getInput('debug')
-  const userAgent = core.getInput('user-agent')
-  const previews = core.getInput('previews')
-
-  const opts: Options = {}
-  if (debug === 'true') opts.log = console
-  if (userAgent != null) opts.userAgent = userAgent
-  if (previews != null) opts.previews = previews.split(',')
-
-  const github = getOctokit(token, opts)
-
   core.info(`argv ${JSON.stringify(process.argv)}`)
 
-  const fn = await import(`./${process.argv[1]}.ts`)
+  const token = process.argv[1]
+  const fnName = process.argv[2]
+  const github = getOctokit(token)
+
+  const fn = await import(`./${fnName}.ts`)
   fn(github, context, core, io)
 }
 
