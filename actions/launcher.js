21ca2294dd8dcbcff071497496e68304fe257d0b
sinoon <sinoon1218@gmail.com>
2020-08-04 16:13:25 +0800

    chore(debug): debug info

--- actions/launcher.js
+++ actions/launcher.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires,no-global-assign
-require = require('esm')()
+require = require('esm')(module)
 require('esbuild-register/register')
 
 module.exports = (path, github, context, core) => {
