9abeee41d6e1c030dab253c41a3363fc6c2714d5
Zheeeng <1303154+zheeeng@users.noreply.github.com>
2020-08-05 09:16:49 +0000

    feat(question): add #57 - Get Required

---
+++ questions/57-hard-get-required/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<GetRequired<{ foo: number, bar?: string }>, { foo: number }>>,
+    Expect<Equal<GetRequired<{ foo: undefined, bar?: undefined }>, { foo: undefined }>>,
+]
