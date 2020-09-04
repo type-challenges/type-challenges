1a9625fc5ea17afafe1bbd70f5764855fb4fa24b
Anthony Fu <11247099+antfu@users.noreply.github.com>
2020-09-04 06:25:59 +0000

    feat(question): add #110 - Capitalize<T>

---
+++ questions/110-medium-capitalize/test-cases.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Capitalize<'foobar'>, 'Foobar'>>,
+  Expect<Equal<Capitalize<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<Capitalize<'foo bar'>, 'Foo bar'>>,
+  Expect<Equal<Capitalize<''>, ''>>,
+]
