db4577863d2521eb784b86a94874d99fb4eddb93
Anthony Fu <11247099+antfu@users.noreply.github.com>
2020-09-04 06:20:24 +0000

    feat(question): add #108 - Trim<T>

---
+++ questions/108-medium-trim/test-cases.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+]
