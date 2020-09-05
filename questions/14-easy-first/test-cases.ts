27cd5354d04a8b459f7de5bb75cd3dc7350e70d5
Anthony Fu <anthonyfu117@hotmail.com>
2020-09-06 02:39:48 +0800

    test(14): fix #137

--- questions/14-easy-first/test-cases.ts
+++ questions/14-easy-first/test-cases.ts
@@ -3,5 +3,6 @@ import { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<First<[3, 2, 1]>, 3>>,
   Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
-  Expect<Equal<First<[]>, never>>
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
 ]
