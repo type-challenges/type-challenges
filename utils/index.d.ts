0b01e0317d8c0dcf203cc91a82aa4d34ab891b59
Anthony Fu <anthonyfu117@hotmail.com>
2020-09-07 11:05:18 +0800

    feat(utils): new util Alike

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -14,6 +14,12 @@ export type IsAny<T> = 0 extends (1 & T) ? true : false
 export type NotAny<T> = true extends IsAny<T> ? false : true
 
 export type Debug<T> = { [K in keyof T]: T[K] }
+export type MergeInsertions<T> =
+  T extends object
+    ? { [K in keyof T]: MergeInsertions<T[K]> }
+    : T
+
+export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>
 
 export type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false
 export type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> = ARGS extends Parameters<FUNC>
