7cd36ef806411043be3f85ad7c4170f6ec35d468
Anthony Fu <11247099+antfu@users.noreply.github.com>
2020-09-11 15:36:37 +0000

    feat(question): add #195 - Reverse Tuple

---
+++ questions/195-medium-reverse-tuple/README.md
@@ -0,0 +1,1 @@
+Implement a generic that takes a tuple and returns a new tuple with the elements in reversed order.
---
+++ questions/195-medium-reverse-tuple/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<[1]>, [1]>>,
+  Expect<Equal<Reverse<[1, 2]>, [2, 1]>>,
+  Expect<Equal<Reverse<[1, undefined, '3']>, ['3', undefined, 1]>>,
+  Expect<Equal<Reverse<[[1, 2], [3, 4]]>, [[3, 4], [1, 2]]>>,
+]
