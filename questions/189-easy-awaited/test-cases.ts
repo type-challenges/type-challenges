4a6e6a0633082d7923fc96d230fed1f8a999dfc9
沧浪 <6065488+sinoon@users.noreply.github.com>
2020-09-17 19:12:39 +0000

    feat(question): add #189 - Awaited

---
+++ questions/189-easy-awaited/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+
+type cases = [
+  Expect<Equal<Awaited<X>, string>>,
+  Expect<Equal<Awaited<Y>, { field: number }>>,
+]
