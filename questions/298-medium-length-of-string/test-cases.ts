3a8b1b98a70fc49ec94fe52f7fc2284f2be67433
Pig Fang <17216317+g-plane@users.noreply.github.com>
2020-10-05 02:02:56 +0000

    feat(question): add #298 - Length of String

---
+++ questions/298-medium-length-of-string/test-cases.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+]
