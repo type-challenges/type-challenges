66dca95d449b769a49dd5fbe490e0c033eeb3f16
H <andywangsy@gmail.com>
2020-10-29 04:33:37 +0000

    feat: add more test cases for question 8

--- questions/8-medium-readonly-2/test-cases.ts
+++ questions/8-medium-readonly-2/test-cases.ts
@@ -3,10 +3,17 @@ import { Alike, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
   Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
 ]
 
 interface Todo1 {
   title: string
+  description?: string
+  completed: boolean
+}
+
+interface Todo2 {
+  readonly title: string
   description?: string
   completed: boolean
 }
