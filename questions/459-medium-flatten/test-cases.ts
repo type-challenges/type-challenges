cc0bd7aaad23e0ab072f2c4c3a43b3777ad05d0b
zhouyiming <16394391+chbro@users.noreply.github.com>
2020-12-09 06:17:55 +0000

    feat(question): add #459 - Flatten

---
+++ questions/459-medium-flatten/test-cases.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type arr = [1, [2]]
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[5]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<Flatten<[{foo: 'bar', 2: 10}, 'foobar']>, [{foo: 'bar', 2: 10}, 'foobar']>>,
+]
