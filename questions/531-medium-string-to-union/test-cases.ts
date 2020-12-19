f128172702b9d5490d659e0a27482ef28362fd6d
Andrey Krasovsky <12611926+bre30kra69cs@users.noreply.github.com>
2020-12-19 04:40:35 +0000

    feat(question): add #531 - String to Union

---
+++ questions/531-medium-string-to-union/test-cases.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StrintToUnion<"">, never>>,
+  Expect<Equal<StrintToUnion<"t">, "t">>,
+  Expect<Equal<StrintToUnion<"hello">, "h" | "e" | "l" | "l" | "o">>,
+  Expect<Equal<StrintToUnion<"coronavirus">, "c" | "o" | "r" | "o" | "n" | "a" | "v" | "i" | "r" | "u" | "s">>,
+]
