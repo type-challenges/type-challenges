8695e554c224fa419f15b2daf9cfbe37da9a2458
Johnson Chu <16279759+johnsoncodehk@users.noreply.github.com>
2021-01-05 14:06:24 +0000

    feat(question): add #610 - CamelCase

---
+++ questions/610-medium-camelcase/template.ts
@@ -0,0 +1,1 @@
+type CamelCase<S> = any;
---
+++ questions/610-medium-camelcase/test-cases.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
+  Expect<Equal<CamelCase<'foo-bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<CamelCase<'foo--bar'>, 'foo-Bar'>>,
+  Expect<Equal<CamelCase<'a-b-c'>, 'aBC'>>,
+  Expect<Equal<CamelCase<'a-b-c-'>, 'aBC-'>>,
+  Expect<Equal<CamelCase<'ABC'>, 'ABC'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+]
