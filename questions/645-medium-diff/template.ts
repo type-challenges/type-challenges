d747217eb3203396189d303748f0f1a25474f640
xiansheng lu <xianshenglu@foxmail.com>
2021-02-21 20:04:37 +0800

    fix(Diff): remove demo code in template.ts (#958)
    * fix(Diff): remove demo code in template.ts
    
    demo code causes `Duplicate identifier bug` in typescript playground
    
    * chore(Diff): change generic param names

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,12 +1,1 @@
 type Diff<O, O1> = any
-
-type Foo = {
-  name: string
-  age: string
-}
-type Bar = {
-  name: string
-  age: string
-  gender: number
-}
-type test0 = Diff<Foo, Bar>   // { gender: number }
