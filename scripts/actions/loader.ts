4a964c122fa741bef993d297d99d25d5a0adcc11
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-03 12:14:23 +0800

    chore: update

--- scripts/actions/loader.ts
+++ scripts/actions/loader.ts
@@ -11,7 +11,7 @@ async function main(): Promise<void> {
   const github = getOctokit(token)
 
   const fn = require(`./${fnName}.ts`)
-  fn(github, context, core, io)
+  fn.default(github, context, core, io)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
