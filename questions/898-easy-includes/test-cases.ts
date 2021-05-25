c539aaa37930c0112033c95456c77c977b922656
ShikChen <ShikChen@users.noreply.github.com>
2021-05-26 01:05:30 +0800

    fix(898): update test cases
    * Remove unused imports.
    * Add one more true case where the target element is not the first one.

--- questions/898-easy-includes/test-cases.ts
+++ questions/898-easy-includes/test-cases.ts
@@ -1,7 +1,8 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
-  Expect<Equal<Includes<['Kars','Esidisi','Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi','Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
 ]
