a6c0e5005169fdfe86723cbad51968d061039863
Yash Singh <saiansh2525@gmail.com>
2021-05-27 16:48:45 -0700

    Add test cases for element in between or at start (#1557)

--- questions/898-easy-includes/test-cases.ts
+++ questions/898-easy-includes/test-cases.ts
@@ -5,4 +5,6 @@ type cases = [
   Expect<Equal<Includes<['Kars', 'Esidisi','Wamuu', 'Santana'], 'Dio'>, false>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
 ]
