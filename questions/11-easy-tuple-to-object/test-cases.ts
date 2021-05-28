ad6b0a53de092d706de7169d1d3491e75ea092a1
Yash Singh <saiansh2525@gmail.com>
2021-05-28 12:26:41 -0700

    Expect error if tuple is not property key (#1560)

--- questions/11-easy-tuple-to-object/test-cases.ts
+++ questions/11-easy-tuple-to-object/test-cases.ts
@@ -5,3 +5,6 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y'}>>,
 ]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
