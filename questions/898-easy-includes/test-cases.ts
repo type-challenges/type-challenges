afd4096561557b416246b1ddc4b56d49fe45a51d
ivorfang <415849035@qq.com>
2021-07-03 11:04:53 +0800

    Add test case for 898 (#2075)

--- questions/898-easy-includes/test-cases.ts
+++ questions/898-easy-includes/test-cases.ts
@@ -9,4 +9,5 @@ type cases = [
   Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
   Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
   Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
 ]
