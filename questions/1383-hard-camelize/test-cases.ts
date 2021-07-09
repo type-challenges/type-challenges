6b7c4bcb91b211f553d20166a85a001c56cb12e3
zongzi531 <zongzi.xy@gmail.com>
2021-07-09 09:02:26 +0800

    Add test case for 1383

--- questions/1383-hard-camelize/test-cases.ts
+++ questions/1383-hard-camelize/test-cases.ts
@@ -1,12 +1,16 @@
-Camelize<{
-  some_prop: string, 
-  prop: { another_prop: string },
-  array: [{ snake_case: string }]
-}> 
+import { Equal, Expect } from '@type-challenges/utils'
 
-// expected to be
-// {
-//   someProp: string, 
-//   prop: { anotherProp: string },
-//   array: [{ snakeCase: string }]
-// }
+type cases = [
+  Expect<Equal<
+  Camelize<{
+    some_prop: string, 
+    prop: { another_prop: string },
+    array: [{ snake_case: string }]
+  }>,
+  {
+    someProp: string, 
+    prop: { anotherProp: string },
+    array: [{ snakeCase: string }]
+  }
+  >>
+]
