5a40f004744e1996a5ebe5d55f2522a16e290c0d
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-08-18 04:05:12 +0000

    feat(question): add #2693 - EndsWith

---
+++ questions/2693-medium-endswith/template.ts
@@ -0,0 +1,1 @@
+type EndsWith<T extends string, U extends string> = any
---
+++ questions/2693-medium-endswith/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<EndsWith<'abc', 'bc'>, true>>,
+  Expect<Equal<EndsWith<'abc', 'abc'>, true>>,
+  Expect<Equal<EndsWith<'abc', 'd'>, false>>,
+]
