386e260b6da18e18b15e81fd84818d88baecbaee
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-08-19 12:51:08 +0000

    feat(question): add #2759 - RequiredByKeys

---
+++ questions/2759-medium-requiredbykeys/README.md
@@ -0,0 +1,16 @@
+Implement a generic `RequiredByKeys<T,  K>` which takes two type argument `T` and `K`.
+
+`K` specify the set of properties of `T` that should set to be required. When `K` is not provided, it should make all properties required just like the normal `Required<T>`.
+
+For example
+
+```typescript
+interface User {
+  name?: string
+  age?: number
+  address?: string
+}
+
+type UserPartialName = RequiredByKeys<User, 'name'> // { name: string; age?: number; address?: string }
+
+```
---
+++ questions/2759-medium-requiredbykeys/test-cases.ts
@@ -0,0 +1,26 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface User {
+  name?: string
+  age?: number
+  address?: string
+}
+
+interface UserRequiredName {
+  name: string
+  age?: number
+  address?: string 
+}
+
+interface UserRequiredNameAndAge {
+  name: string
+  age: number
+  address?: string 
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, 'name'>, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, 'name' | 'unknown'>, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, 'name' | 'age'>, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+]
