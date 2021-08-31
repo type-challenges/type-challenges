15b57c0c6a908231964b71c1e704cd93c5b022cb
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-08-31 09:43:40 +0000

    feat(question): add #2946 - ObjectEntries

---
+++ questions/2946-medium-objectentries/test-cases.ts
@@ -0,0 +1,16 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null];
+
+
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>,ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>,ModelEntries>>,
+]
