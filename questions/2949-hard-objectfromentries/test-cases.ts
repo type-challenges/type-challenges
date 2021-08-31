274f76771f402455b28639ca6d65f681bc9e8e5d
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-08-31 10:12:29 +0000

    feat(question): add #2949 - ObjectFromEntries

---
+++ questions/2949-hard-objectfromentries/test-cases.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null];
+
+
+type cases = [
+  Expect<Equal<ObjectFromEntries<ModelEntries>,Model>>,
+]
