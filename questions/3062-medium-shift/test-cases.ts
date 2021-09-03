1f9779fd88b8918b6ce9cb6d8fff3a7ffb2fb99d
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-09-03 03:55:35 +0000

    feat(question): add #3062 - Shift

---
+++ questions/3062-medium-shift/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd', ]>, ['b', 'c', 'd']>>,
+]
