d7167f075410372713eb17de2cb2f5782a2efbba
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-09-03 03:37:41 +0000

    feat(question): add #3060 - Unshift

---
+++ questions/3060-easy-unshift/template.ts
@@ -0,0 +1,1 @@
+type Unshift<T, U> = any
---
+++ questions/3060-easy-unshift/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2,]>>,
+  Expect<Equal<Unshift<['1', 2, '3'],boolean>, [boolean, '1', 2, '3']>>,
+]
