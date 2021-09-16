04e1b11f328bb3553cd3071e56c1dc2b63634871
yorisilo <10104705+yorisilo@users.noreply.github.com>
2021-09-16 15:49:28 +0000

    feat(question): add #3309 - Shift

---
+++ questions/3309-easy-shift/README.md
@@ -0,0 +1,1 @@
+Implement the type version of `Array.shift`
---
+++ questions/3309-easy-shift/test-cases.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Shift<[1]>, 1>>,
+  Expect<Equal<Shift<[1, 2, 3]>, 1>>,
+  Expect<Equal<Shift<['a', 'b', 'c']>, 'a'>>,
+  Expect<Equal<Shift<[1, '2', 3]>, 1>>,
+  Expect<Equal<Shift<[boolean, 1, 'a']>, boolean>>,
+  Expect<Equal<Shift<[{a: '1'}, {}]>, {a: '1'}>>,
+  Expect<Equal<Shift<[]>, never>>,
+]
