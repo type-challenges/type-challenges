dadd594c7ce4fb3fa066ac500fa9e2bd37fbe0de
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-10-20 07:42:27 +0000

    feat(question): add #4037 - IsPalindrome

---
+++ questions/4037-hard-ispalindrome/template.ts
@@ -0,0 +1,1 @@
+type IsPalindrome<T> = any
---
+++ questions/4037-hard-ispalindrome/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsPalindrome<'abc'>, false>>,
+  Expect<Equal<IsPalindrome<'b'>, true>>,
+  Expect<Equal<IsPalindrome<'abca'>, false>>,
+  Expect<Equal<IsPalindrome<121>, true>>,
+  Expect<Equal<IsPalindrome<19260817>, false>>,
+]
