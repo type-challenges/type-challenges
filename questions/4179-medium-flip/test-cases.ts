8df892f29357dfac5ef43ce79219704ff5951de4
Farhan Kathawala <9617471+kathawala@users.noreply.github.com>
2021-10-31 17:25:44 +0000

    feat(question): add #4179 - Flip

---
+++ questions/4179-medium-flip/test-cases.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<{a: 'pi'}, Flip<{pi: 'a'}>>>,
+  Expect<NotEqual<{b: 'pi'}, Flip<{pi: 'a'}>>>,
+  Expect<Equal<{3.14: 'pi', true: 'bool'}, Flip<{pi: 3.14, bool: true}>>>,
+  Expect<Equal<{val2: 'prop2', val: 'prop'}, Flip<{prop: 'val', prop2: 'val2'}>>>,
+]
