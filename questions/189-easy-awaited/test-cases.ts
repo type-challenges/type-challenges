2349792be7ad0f27e07fc536a4141c4297e78fd5
jiangshan <540118044@qq.com>
2021-11-22 12:56:37 +0800

    fix: add text case for Awaited (#4623)

--- questions/189-easy-awaited/test-cases.ts
+++ questions/189-easy-awaited/test-cases.ts
@@ -2,10 +2,12 @@ import { Equal, Expect } from '@type-challenges/utils'
 
 type X = Promise<string>
 type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
 
 type cases = [
   Expect<Equal<Awaited<X>, string>>,
   Expect<Equal<Awaited<Y>, { field: number }>>,
+  Expect<Equal<Awaited<Z>, string | number>>,
 ]
 
 // @ts-expect-error
