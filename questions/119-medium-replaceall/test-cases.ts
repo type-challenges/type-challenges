8451ac5f4008c9d49a9437f9d170da9ffa5f9160
Li Kui <90845831+likui628@users.noreply.github.com>
2021-11-30 13:16:44 +0800

    fix: add test case for #119 medium replaceall (#4792)

--- questions/119-medium-replaceall/test-cases.ts
+++ questions/119-medium-replaceall/test-cases.ts
@@ -2,6 +2,7 @@ import { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
   Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
   Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
   Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
