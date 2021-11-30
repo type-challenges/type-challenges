0d30b866c4c0268801c4887e8ae46e4342492407
yugang.cao <yugang.cao@tusimple.ai>
2021-11-30 16:47:49 +0800

    feat: add trimRight type

---
+++ questions/107-medium-trimright/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimRight<'str'>, 'str'>>,
+  Expect<Equal<TrimRight<'str '>, 'str'>>,
+  Expect<Equal<TrimRight<'str     '>, 'str'>>,
+  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
+  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
+]
