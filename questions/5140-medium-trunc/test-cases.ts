8b42ba23683c999657dee47d0f2153a5428425c0
jiangshan <13798212+jiangshanmeta@users.noreply.github.com>
2021-12-15 08:18:42 +0000

    feat(question): add #5140 - Trunc

---
+++ questions/5140-medium-trunc/test-cases.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trunc<0.1>, '0'>>,
+  Expect<Equal<Trunc<1.234>, '1'>>,
+  Expect<Equal<Trunc<12.345>, '12'>>,
+  Expect<Equal<Trunc<-5.1>, '-5'>>,
+  Expect<Equal<Trunc<'1.234'>, '1'>>,
+  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
+  Expect<Equal<Trunc<10>, '10'>>,
+]
