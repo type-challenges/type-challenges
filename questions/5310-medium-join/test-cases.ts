3c9eef2db3f492582d1fffb7f384d38ed6626287
Pineapple <74694190+Pineapple0919@users.noreply.github.com>
2021-12-21 15:54:21 +0000

    feat(question): add #5310 - Join

---
+++ questions/5310-medium-join/test-cases.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Join<["a", "p", "p", "l", "e"], "-">, "a-p-p-l-e">>,
+  Expect<Equal<Join<["Hello", "World"], " ">, "Hello World">>,
+  Expect<Equal<Join<["2", "2", "2"], 1>, "21212">>，
+  Expect<Equal<Join<["o"], "u">, "o">>
+]
