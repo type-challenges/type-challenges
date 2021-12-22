e48c2c60c36f25b21ffaf1e6caf295e454c36072
ch3cknull <ch3cknull@outlook.com>
2021-12-22 19:50:59 +0800

    fix: typo in 5310 test case

--- questions/5310-medium-join/test-cases.ts
+++ questions/5310-medium-join/test-cases.ts
@@ -3,6 +3,6 @@ import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Join<["a", "p", "p", "l", "e"], "-">, "a-p-p-l-e">>,
   Expect<Equal<Join<["Hello", "World"], " ">, "Hello World">>,
-  Expect<Equal<Join<["2", "2", "2"], 1>, "21212">>，
+  Expect<Equal<Join<["2", "2", "2"], 1>, "21212">>,
   Expect<Equal<Join<["o"], "u">, "o">>
 ]
