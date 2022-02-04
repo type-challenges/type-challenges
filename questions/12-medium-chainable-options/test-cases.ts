c5c2b22071f29a3e65a734ced6cfa58fdf6135f1
oceanlvr <36698124+oceanlvr@users.noreply.github.com>
2022-02-04 09:19:32 +0800

    feat(12): add test case (#6262)

--- questions/12-medium-chainable-options/test-cases.ts
+++ questions/12-medium-chainable-options/test-cases.ts
@@ -2,20 +2,31 @@ import { Alike, Expect } from '@type-challenges/utils'
 
 declare const a: Chainable
 
-const result = a
+const result1 = a
   .option('foo', 123)
   .option('bar', { value: 'Hello World' })
   .option('name', 'type-challenges')
   .get()
 
+const result2 = a
+  .option('name', 'another name')
+  // @ts-expect-error
+  .option('name', 'last name')
+  .get()
+
 type cases = [
-  Expect<Alike<typeof result, Expected>>
+  Expect<Alike<typeof result1, Expected1>>,
+  Expect<Alike<typeof result2, Expected2>>,
 ]
 
-type Expected = {
+type Expected1 = {
   foo: number
   bar: {
     value: string
   }
+  name: string
+}
+
+type Expected2 = {
   name: string
 }
