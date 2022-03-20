a7b89a5f402cde8eb397a57d8b59c78545a5fae1
liutian <809143962@qq.com>
2022-03-20 16:56:47 +0800

    enhance: add test cases to avoid duplication (#7611)

--- questions/3057-easy-push/test-cases.ts
+++ questions/3057-easy-push/test-cases.ts
@@ -4,4 +4,5 @@ type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
   Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
   Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+  Expect<Equal<Push<['1', 2, '3'], '3'>, ['1', 2, '3']>>,
 ]
