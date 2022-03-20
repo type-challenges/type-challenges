5f87cbbef1575420f1a12967503ea9a87b7edca4
liutian <809143962@qq.com>
2022-03-20 23:16:37 +0800

    enhance: add test cases to 3060-easy-unshift (#7624)

--- questions/3060-easy-unshift/test-cases.ts
+++ questions/3060-easy-unshift/test-cases.ts
@@ -3,5 +3,6 @@ import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Unshift<[], 1>, [1]>>,
   Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2,]>>,
-  Expect<Equal<Unshift<['1', 2, '3'],boolean>, [boolean, '1', 2, '3']>>,
+  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
+  Expect<Equal<Unshift<['1', 2, '3'], '3'>, ['1', 2, '3']>>,
 ]
