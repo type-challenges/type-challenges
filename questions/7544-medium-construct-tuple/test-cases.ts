f744fce7b7d667a61e1a8796cc1899f92c1d0d55
Lo <50254496+LoTwT@users.noreply.github.com>
2022-03-22 04:23:36 +0000

    feat(question): add #7544 - Construct Tuple

---
+++ questions/7544-medium-construct-tuple/test-cases.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<ConstructTuple<0>, []>>,
+  Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
+  Expect<Equal<ConstructTuple<999>["length"], 999>>,
+  // @ts-expect-error
+  Expect<Equal<ConstructTuple<1000>["length"], 1000>>,
+]
