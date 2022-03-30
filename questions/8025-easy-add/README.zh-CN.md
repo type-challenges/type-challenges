247cb51c2b4f1276c39f2a500f2d835e39908649
daixiongsheng <28498805+daixiongsheng@users.noreply.github.com>
2022-03-30 05:17:20 +0000

    feat(question): add #8025 - Add

---
+++ questions/8025-easy-add/README.zh-CN.md
@@ -0,0 +1,5 @@
+```ts
+
+  type R1 = Add<1, 2> // expected to be 3
+  type R2 = Add<100, 100> // expected to be 200
+  ```
---
+++ questions/8025-easy-add/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Add<1, 2>, 3>>
+  Expect<Equal<Add<100, 100>, 200>>
+]
