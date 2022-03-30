858fab61b16dc25bd4e16310724919305eb5b700
daixiongsheng <28498805+daixiongsheng@users.noreply.github.com>
2022-03-30 05:24:02 +0000

    feat(question): add #8031 - Pow

---
+++ questions/8031-medium-pow/README.zh-CN.md
@@ -0,0 +1,6 @@
+```ts
+type A = Pow<3, 3> // 27
+type B = Pow<10, 2> // 100
+type C = Pow<10, 0> // 1
+type D = Pow<10, 1> // 10
+```
---
+++ questions/8031-medium-pow/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Pow<3, 3>, 27>>,
+  Expect<Equal<Pow<10, 0>, 1>>,
+  Expect<Equal<Pow<2, 10>, 1024>>
+]
