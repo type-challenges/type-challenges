0c594c1059103b705239e71dad6fa646625130bb
daixiongsheng <28498805+daixiongsheng@users.noreply.github.com>
2022-03-30 05:20:12 +0000

    feat(question): add #8027 - Divide

---
+++ questions/8027-medium-divide/README.zh-CN.md
@@ -0,0 +1,5 @@
+```TS
+type A = Divide<10, 3> // 3
+type B = Divide<10, 2> // 5
+type C = Divide<10, 0> // never
+```
---
+++ questions/8027-medium-divide/test-cases.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Divide<10, 3>, 3>>,
+  Expect<Equal<Divide<10, 2>, 5>>,
+  Expect<Equal<Divide<10, 0>, never>>
+]
