c55982c1b1cae90849d569af0a0d95e7476a5fc5
N. Bighetti <60653216+tenkirin@users.noreply.github.com>
2022-04-01 00:10:02 +0800

    enhance: add test cases to Trim & TrimRight (#8124)

--- questions/108-medium-trim/test-cases.ts
+++ questions/108-medium-trim/test-cases.ts
@@ -7,4 +7,6 @@ type cases = [
   Expect<Equal<Trim<'str   '>, 'str'>>,
   Expect<Equal<Trim<'     str     '>, 'str'>>,
   Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
 ]
