b689b0c2cedd6d0622fc30935ecd50af6fec4ba1
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-11 12:50:14 +0000

    feat(question): add #8536 - HighDimensionalMapRaw

---
+++ questions/8536-extreme-highdimensionalmapraw/README.zh-CN.md
@@ -0,0 +1,13 @@
+在这里描述你的题目并给出一些例子。支持使用 Markdown。
+```ts
+export let a:HighDimensionalMapRaw<number,number, 4>=new Map()
+export let b:Map<number,Map<number,Map<number,Map<number,number>>>>=new Map()
+
+
+
+
+
+a=b
+b=a
+
+```
---
+++ questions/8536-extreme-highdimensionalmapraw/test-cases.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect, ExpectFalse, NotEqual,ExpectExtends } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<HighDimensionalMapRaw<number,number, 4> , Map<number,Map<number,Map<number,Map<number,number>>>>>>,
+   Expect<Equal< Map<number,Map<number,Map<number,Map<number,number>>>>,HighDimensionalMapRaw<number,number, 4> >>,
+
+Expect<Equal<HighDimensionalMapRaw<number,number, 3> , Map<number,Map<number,Map<number,number>>>>>,
+   Expect<Equal< Map<number,Map<number,Map<number,Map<number,Map<number,number>>>>>,HighDimensionalMapRaw<number,number, 5> >>,
+
+
+]
