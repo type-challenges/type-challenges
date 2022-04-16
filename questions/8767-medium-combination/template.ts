8ae5013d81edf18226b354b6d4e2fbeed04b6574
HomyeeKing <HomyeeKing@gmail.com>
2022-04-16 17:09:43 +0800

    fix: template & test-cases

---
+++ questions/8767-medium-combination/template.ts
@@ -0,0 +1,1 @@
+type Combination<T extends string[]> = any
--- questions/8767-medium-combination/test-cases.ts
+++ questions/8767-medium-combination/test-cases.ts
@@ -2,5 +2,5 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<PermutationCombination<['foo', 'bar', 'baz']>,
-  'foo' | 'bar' | 'baz' | 'foo bar baz' | 'bar foo' | 'baz foo' | 'bar baz' | 'baz bar'>>,
+  'foo' | 'bar' | 'baz' | 'foo bar baz' | 'bar foo' | 'baz foo' | 'baz bar'>>,
 ]
