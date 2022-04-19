5f51060a8799536d2f5b5682d53b8dd6a916783c
ch3cknull <ch3cknull@outlook.com>
2022-04-19 13:17:19 +0800

    typo(8767): type name in test case (#8953)
    update type name in test case that same to template and title

--- questions/08767-medium-combination/test-cases.ts
+++ questions/08767-medium-combination/test-cases.ts
@@ -1,6 +1,6 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<PermutationCombination<['foo', 'bar', 'baz']>,
+  Expect<Equal<Combination<['foo', 'bar', 'baz']>,
   'foo' | 'bar' | 'baz' | 'foo bar baz' | 'bar foo' | 'baz foo' | 'baz bar'>>,
 ]
