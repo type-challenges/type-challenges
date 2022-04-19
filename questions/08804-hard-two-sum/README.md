d2fdcb924dc88a2df83c2406233770f2bd3243f6
PsiloLau <25545052+Psilocine@users.noreply.github.com>
2022-04-19 06:37:17 +0000

    feat(question): add #8804 - Two Sum

---
+++ questions/08804-hard-two-sum/README.md
@@ -0,0 +1,1 @@
+Given an array of integers `nums` and an integer `target`, return true if two numbers such that they add up to `target`.
---
+++ questions/08804-hard-two-sum/test-cases.ts
@@ -0,0 +1,8 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TwoSum<[3, 3], 6>, true>>,
+  Expect<Equal<TwoSum<[3, 2, 4], 6>, true>>,
+  Expect<Equal<TwoSum<[2, 7, 11, 15], 15>, false>>,
+  Expect<Equal<TwoSum<[2, 7, 11, 15], 9>, true>>
+]
