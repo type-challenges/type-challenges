982ea844a18aa657331028cda1769d4290bbd03d
tenkirin <60653216+tenkirin@users.noreply.github.com>
2022-04-20 15:38:49 +0800

    enhance: improve test cases in 01978-medium-percentage-parser

--- questions/01978-medium-percentage-parser/test-cases.ts
+++ questions/01978-medium-percentage-parser/test-cases.ts
@@ -1,17 +1,29 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type Case1 = ['', '', '']
-type Case2 = ['+', '', '']
-type Case3 = ['+', '1', '']
+type Case0 = ['', '', '']
+type Case1 = ['+', '', '']
+type Case2 = ['+', '1', '']
+type Case3 = ['+', '100', '']
 type Case4 = ['+', '100', '%']
-type Case5 = ['', '10', '%']
-type Case6 = ['-', '99', '%']
+type Case5 = ['', '100', '%']
+type Case6 = ['-', '100', '%']
+type Case7 = ['-', '100', '']
+type Case8 = ['-', '1', '']
+type Case9 = ['', '', '%']
+type Case10 = ['', '1', '']
+type Case11 = ['', '100', '']
 
 type cases = [
-  Expect<Equal<PercentageParser<''>, Case1>>,
-  Expect<Equal<PercentageParser<'+'>, Case2>>,
-  Expect<Equal<PercentageParser<'+1'>, Case3>>,
+  Expect<Equal<PercentageParser<''>, Case0>>,
+  Expect<Equal<PercentageParser<'+'>, Case1>>,
+  Expect<Equal<PercentageParser<'+1'>, Case2>>,
+  Expect<Equal<PercentageParser<'+100'>, Case3>>,
   Expect<Equal<PercentageParser<'+100%'>, Case4>>,
-  Expect<Equal<PercentageParser<'10%'>, Case5>>,
-  Expect<Equal<PercentageParser<'-99%'>, Case6>>,
+  Expect<Equal<PercentageParser<'100%'>, Case5>>,
+  Expect<Equal<PercentageParser<'-100%'>, Case6>>,
+  Expect<Equal<PercentageParser<'-100'>, Case7>>,
+  Expect<Equal<PercentageParser<'-1'>, Case8>>,
+  Expect<Equal<PercentageParser<'%'>, Case9>>,
+  Expect<Equal<PercentageParser<'1'>, Case10>>,
+  Expect<Equal<PercentageParser<'100'>, Case11>>,
 ]
