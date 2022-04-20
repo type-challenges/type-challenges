83e6a953c12c2cde8720464d2a24300fe8eb3c0e
Lo <709937065@qq.com>
2022-04-20 11:22:56 +0800

    test: add test cases to 08804-hard-two-sum (#9000)

--- questions/08804-hard-two-sum/test-cases.ts
+++ questions/08804-hard-two-sum/test-cases.ts
@@ -5,4 +5,11 @@ type cases = [
   Expect<Equal<TwoSum<[3, 2, 4], 6>, true>>,
   Expect<Equal<TwoSum<[2, 7, 11, 15], 15>, false>>,
   Expect<Equal<TwoSum<[2, 7, 11, 15], 9>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 0>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 1>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 2>, false>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 3>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 4>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 5>, true>>,
+  Expect<Equal<TwoSum<[1, 2, 3], 6>, false>>
 ]
