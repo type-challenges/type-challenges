8ef5e7bdca6ae15c6cf609a9fc96df6ac6ee68af
ch3cknull <45302021+ch3cknull@users.noreply.github.com>
2022-04-23 15:47:28 +0000

    feat(question): add #9155 - ValidDate

---
+++ questions/09155-hard-validdate/README.md
@@ -0,0 +1,15 @@
+Implement a type `ValidDate`, which takes an input type T and returns whether T is a valid date.
+
+**Leap year is not considered**
+
+Good Luck!
+
+```ts
+ValidDate<'0102'> // true
+ValidDate<'0131'> // true
+ValidDate<'1231'> // true
+ValidDate<'0229'> // false
+ValidDate<'0100'> // false
+ValidDate<'0132'> // false
+ValidDate<'1301'> // false
+```
---
+++ questions/09155-hard-validdate/test-cases.ts
@@ -0,0 +1,11 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<ValidDate<'0102'>, true>>,
+  Expect<Equal<ValidDate<'0131'>, true>>,
+  Expect<Equal<ValidDate<'1231'>, true>>,
+  Expect<Equal<ValidDate<'0229'>, false>>,
+  Expect<Equal<ValidDate<'0100'>, false>>,
+  Expect<Equal<ValidDate<'0132'>, false>>,
+  Expect<Equal<ValidDate<'1301'>, false>>,
+]
