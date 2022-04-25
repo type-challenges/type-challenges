4859bfd38fe91d08316c81a76c17cbbd0faf96f2
RThong <403216075@qq.com>
2022-04-25 19:50:24 +0800

    enhance: improve test cases in 04425-medium-greater-than

--- questions/04425-medium-greater-than/test-cases.ts
+++ questions/04425-medium-greater-than/test-cases.ts
@@ -1,4 +1,4 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
 type cases = [
   Expect<Equal<GreaterThan<1, 0>, true>>,
@@ -6,4 +6,6 @@ type cases = [
   Expect<Equal<GreaterThan<4, 5>, false>>,
   Expect<Equal<GreaterThan<0, 0>, false>>,
   Expect<Equal<GreaterThan<20, 20>, false>>,
+  Expect<Equal<GreaterThan<10, 100>, false>>,
+  Expect<Equal<GreaterThan<111, 11>, true>>
 ]
