08bb314904fa73f01cc08193027595c16fe977d8
Hong <403216075@qq.com>
2022-04-27 19:51:57 +0800

    enhance: improve test cases in 05153-medium-indexof and 05317-medium-lastindexof (#9434)

--- questions/05317-medium-lastindexof/test-cases.ts
+++ questions/05317-medium-lastindexof/test-cases.ts
@@ -5,4 +5,6 @@ type cases = [
   Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
   Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
   Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<LastIndexOf<[string, 2, number, 'a', number, 1], number>, 4>>,
+  Expect<Equal<LastIndexOf<[string, any, 1, number, 'a', any, 1], any>, 5>>
 ]
