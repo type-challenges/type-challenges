3b25f515f842d49ade0feeb407815c4ac58ba87f
antfu <antfu@users.noreply.github.com>
2022-04-28 00:03:39 +0000

    chore: update index

--- questions/05153-medium-indexof/test-cases.ts
+++ questions/05153-medium-indexof/test-cases.ts
@@ -5,5 +5,5 @@ type cases = [
   Expect<Equal<IndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 2>>,
   Expect<Equal<IndexOf<[0, 0, 0], 2>, -1>>,
   Expect<Equal<IndexOf<[string, 1, number, 'a'], number>, 2>>,
-  Expect<Equal<IndexOf<[string, 1, number, 'a', any], any>, 4>>
+  Expect<Equal<IndexOf<[string, 1, number, 'a', any], any>, 4>>,
 ]
