4cf8751859c64588b7850a44d01bdafc817ea701
Laffery <49607541+Laffery@users.noreply.github.com>
2022-04-30 07:13:06 +0000

    feat(question): add #9535 - void function

---
+++ questions/09535-medium-void-function/test-cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<VoidFunction, () => void>>,
+  Expect<Equal<VoidFunction<boolean>, (arg: boolean) => void>>,
+  Expect<Equal<VoidFunction<[boolean, boolean]>, (arg1: boolean, arg2: boolean) => void>>,
+  Expect<Equal<VoidFunction<[boolean, boolean, boolean]>, (...args: [boolean, boolean, boolean]) => void>>,
+]
