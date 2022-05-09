f63fe7b403fa8959fdd972f7c34a0825b1edc8da
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-05-09 00:59:29 +0000

    feat(question): add #9908 - Two Sum

---
+++ questions/09908-hard-two-sum/README.md
@@ -0,0 +1,3 @@
+Given an tuple of  integer `nums` and an integer target, return a boolean literal type that indicates whether there are two numbers such that they add up to `target`.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
---
+++ questions/09908-hard-two-sum/test-cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TwoSum<[1,2,3,4], 6>, true>>,
+  Expect<Equal<TwoSum<[1,2,3,4], 9>, false>>,
+  Expect<Equal<TwoSum<[1,9], 2>, false>>,
+  Expect<Equal<TwoSum<[1,1,9], 2>, true>>,
+]
