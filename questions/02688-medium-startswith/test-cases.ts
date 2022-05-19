59fcc8997829d4ba409c65516fc4ea2e0df9c19b
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-05-19 23:26:32 +0800

    chore(questions): add Chinese translation & test case

--- questions/02688-medium-startswith/test-cases.ts
+++ questions/02688-medium-startswith/test-cases.ts
@@ -5,4 +5,6 @@ type cases = [
   Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
   Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
   Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', ''>, true>>,
+  Expect<Equal<StartsWith<'abc', ' '>, false>>,
 ]
