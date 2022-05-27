3cc19ee439c95aa9349befa600adee4af18f77f7
Gideon Oludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2022-05-27 01:32:50 +0000

    feat(question): add #1202 - Function Composition LTR

---
+++ questions/01202-medium-function-composition-ltr/README.md
@@ -0,0 +1,8 @@
+Write a generic type that takes a non-empty array of *single-parameter* functions and returns a function
+with the same parameter type and return type as the first and last functions
+in the array respectively, if and only if the parameter type of each function matches
+the return type of the function before it (*left-to-right*).
+
+```ts
+type composed = Compose<[(x: number) => string, (y: string) => boolean]> // (x: number) => boolean
+```
---
+++ questions/01202-medium-function-composition-ltr/test-cases.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Compose<[(x: number) => string, (y: string) => boolean]>, (x: number) => boolean>,
+  Expect<NotEqual<Compose<[(x: boolean) => number, (y: string) => boolean]>, (y: string) => number>>,
+  Expect<NotEqual<Compose<[(x: number) => string, (y: number) => number, (z: number) => boolean]>, (x: number) => boolean>,
+  Expect<Equal<Compose<[(x: number) => string, (y: string) => number, (z: number) => boolean]>, (x: number) => boolean>,
+  Expect<Equal<Compose<[(x: 0) => false]>, (x: 0) => false>,
+  Expect<NotEqual<Compose<[(x: 0) => false, (y: boolean) => 10]>, (x: 0) => 10>>,
+]
