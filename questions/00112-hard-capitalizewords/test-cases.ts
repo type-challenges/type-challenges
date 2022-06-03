9af2d468cf0c104fed9f11acd6ad39c8ba2d52f8
tenkirin <60653216+tenkirin@users.noreply.github.com>
2022-06-03 21:13:39 +0900

    enhance: add a test case to 112-hard-capitalizewords

--- questions/00112-hard-capitalizewords/test-cases.ts
+++ questions/00112-hard-capitalizewords/test-cases.ts
@@ -6,5 +6,6 @@ type cases = [
   Expect<Equal<CapitalizeWords<'foo bar'>, 'Foo Bar'>>,
   Expect<Equal<CapitalizeWords<'foo bar hello world'>, 'Foo Bar Hello World'>>,
   Expect<Equal<CapitalizeWords<'foo bar.hello,world'>, 'Foo Bar.Hello,World'>>,
+  Expect<Equal<CapitalizeWords<'aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq'>, 'Aa!Bb@Cc#Dd$Ee%Ff^Gg&Hh*Ii(Jj)Kk_Ll+Mm{Nn}Oo|Pp🤣Qq'>>,
   Expect<Equal<CapitalizeWords<''>, ''>>,
 ]
