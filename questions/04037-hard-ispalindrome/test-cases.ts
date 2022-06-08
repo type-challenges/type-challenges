e4f05e5f427ab630cd53b289dcf36b7ad99c26c5
Aleksey Bobyr <agat00@gmail.com>
2022-06-08 19:21:54 +0300

    Update test-cases.ts (#11341)

--- questions/04037-hard-ispalindrome/test-cases.ts
+++ questions/04037-hard-ispalindrome/test-cases.ts
@@ -4,6 +4,7 @@ type cases = [
   Expect<Equal<IsPalindrome<'abc'>, false>>,
   Expect<Equal<IsPalindrome<'b'>, true>>,
   Expect<Equal<IsPalindrome<'abca'>, false>>,
+  Expect<Equal<IsPalindrome<'abcba'>, true>>,
   Expect<Equal<IsPalindrome<121>, true>>,
   Expect<Equal<IsPalindrome<19260817>, false>>,
 ]
