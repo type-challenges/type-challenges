76648bcc61f6d96dd2f917ee1c1911a173e62ef1
wenpei <2737716714@qq.com>
2022-06-08 10:25:49 +0800

    type-challenges

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,10 @@
-type First<T extends any[]> = any
+/*
+ * @Author: wenpei 2737716714@qq.com
+ * @Date: 2022-06-07 10:48:24
+ * @LastEditors: wenpei 2737716714@qq.com
+ * @LastEditTime: 2022-06-08 10:25:06
+ * @FilePath: /type-challenges/questions/00014-easy-first/template.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+type First<T extends any[]> = T extends [] ? never : T[0]
+
