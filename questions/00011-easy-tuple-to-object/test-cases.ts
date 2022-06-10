758a4c073b48a4bf294527222ae4fb290beca5cd
lzy1960 <40552111+lzy1960@users.noreply.github.com>
2022-06-10 15:52:45 +0800

    add test-cases (#11363)
    * add 11-tuple-to-object test-case with number type
    
    * add Awaited test-cases

--- questions/00011-easy-tuple-to-object/test-cases.ts
+++ questions/00011-easy-tuple-to-object/test-cases.ts
@@ -1,9 +1,13 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const tupleMix = [1, '2', 3, '4'] as const
 
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
 ]
 
 // @ts-expect-error
--- questions/00189-easy-awaited/test-cases.ts
+++ questions/00189-easy-awaited/test-cases.ts
@@ -3,11 +3,13 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type X = Promise<string>
 type Y = Promise<{ field: number }>
 type Z = Promise<Promise<string | number>>
+type Z1 = Promise<Promise<Promise<string | boolean>>>
 
 type cases = [
   Expect<Equal<MyAwaited<X>, string>>,
   Expect<Equal<MyAwaited<Y>, { field: number }>>,
   Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
 ]
 
 // @ts-expect-error
