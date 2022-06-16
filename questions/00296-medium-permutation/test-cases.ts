810af652156167a5893a7a9e665b738c957be5c8
zz <2418184580@qq.com>
2022-06-16 16:59:33 +0800

    chore(00296-medium-permutation): improve test cases (#11394)

--- questions/00296-medium-permutation/test-cases.ts
+++ questions/00296-medium-permutation/test-cases.ts
@@ -4,5 +4,6 @@ type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
   Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
   Expect<Equal<Permutation<never>, []>>,
 ]
