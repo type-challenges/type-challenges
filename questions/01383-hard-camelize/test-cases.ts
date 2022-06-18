fafe9405dcced2f9d3cdeaac59bd8e3f49a8b3c6
N. Bighetti <60653216+tenkirin@users.noreply.github.com>
2022-06-18 21:14:31 +0900

    enhance: improve test case in 1383-hard-camelize (#11793)

--- questions/01383-hard-camelize/test-cases.ts
+++ questions/01383-hard-camelize/test-cases.ts
@@ -2,15 +2,23 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<
-  Camelize<{
-    some_prop: string
-    prop: { another_prop: string }
-    array: [{ snake_case: string }]
-  }>,
-  {
-    someProp: string
-    prop: { anotherProp: string }
-    array: [{ snakeCase: string }]
-  }
+    Camelize<{
+      some_prop: string
+      prop: { another_prop: string }
+      array: [
+        { snake_case: string },
+        { another_element: { yet_another_prop: string } },
+        { yet_another_element: string },
+      ]
+    }>,
+    {
+      someProp: string
+      prop: { anotherProp: string }
+      array: [
+        { snakeCase: string },
+        { anotherElement: { yetAnotherProp: string } },
+        { yetAnotherElement: string },
+      ]
+    }
   >>,
 ]
