9d78eb6b42e0d6364579a318f0c7e0c2eafefa95
Hansanguk <hansanguk0222@gmail.com>
2022-06-22 23:07:36 +0900

    표현 수정

--- answer/index.ts
+++
@@ -1,9 +0,0 @@
-type KeyValueSameDict<Keys extends string> = {
-  [v in Keys]?: v
-}
-
-function inferGeneric<T extends string>(t: KeyValueSameDict<T>) {
-  return t
-}
-
-const foo = inferGeneric({ foo: "foo", bar: "bar" } as const)
