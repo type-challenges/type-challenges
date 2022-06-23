457387752d5c5831d6e0ab4707217a88c198e100
lzy1960 <40552111+lzy1960@users.noreply.github.com>
2022-06-23 18:42:36 +0800

    test: add ts-expect-error in 3196-flip-arguments (#12130)

--- questions/03196-medium-flip-arguments/test-cases.ts
+++ questions/03196-medium-flip-arguments/test-cases.ts
@@ -5,3 +5,14 @@ type cases = [
   Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
   Expect<Equal<FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>, (arg0: boolean, arg1: number, arg2: string) => void>>,
 ]
+
+type errors = [
+  // @ts-expect-error
+  FlipArguments<'string'>,
+  // @ts-expect-error
+  FlipArguments<{ key: 'value' }>,
+  // @ts-expect-error
+  FlipArguments<['apple', 'banana', 100, { a: 1 }]>,
+  // @ts-expect-error
+  FlipArguments<null | undefined>,
+]
