e7c0839ed5f84403abddac753c820f6d160419e7
wangj <wangj@chinawyny.com>
2022-07-04 15:03:44 +0800

    currying

--- questions/00006-hard-simple-vue/test-cases.ts
+++ questions/00006-hard-simple-vue/test-cases.ts
@@ -1,37 +1,53 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type GetComputed<TComputed> = {
+  [key in keyof TComputed]: TComputed[key] extends () => infer Result
+    ? Result
+    : never;
+};
+
+type Options<TData, TComputed, TMethods> = {
+  data: (this: void) => TData;
+  computed: TComputed & ThisType<TData>;
+  methods: TMethods & ThisType<TData & GetComputed<TComputed> & TMethods>;
+};
+
+declare function SimpleVue<TData, TComputed, TMethods>(
+  options: Options<TData, TComputed, TMethods>,
+): unknown;
 
 SimpleVue({
   data() {
     // @ts-expect-error
-    this.firstname
+    this.firstname;
     // @ts-expect-error
-    this.getRandom()
+    this.getRandom();
     // @ts-expect-error
-    this.data()
+    this.data();
 
     return {
       firstname: 'Type',
       lastname: 'Challenges',
       amount: 10,
-    }
+    };
   },
   computed: {
     fullname() {
-      return `${this.firstname} ${this.lastname}`
+      return `${this.firstname} ${this.lastname}`;
     },
   },
   methods: {
     getRandom() {
-      return Math.random()
+      return Math.random();
     },
     hi() {
-      alert(this.amount)
-      alert(this.fullname.toLowerCase())
-      alert(this.getRandom())
+      alert(this.amount);
+      alert(this.fullname.toLowerCase());
+      alert(this.getRandom());
     },
     test() {
-      const fullname = this.fullname
-      const cases: [Expect<Equal<typeof fullname, string>>] = [] as any
+      const fullname = this.fullname;
+      const cases: [Expect<Equal<typeof fullname, string>>] = [] as any;
     },
   },
-})
+});
--- questions/00017-hard-currying-1/test-cases.ts
+++ questions/00017-hard-currying-1/test-cases.ts
@@ -1,13 +1,40 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
-const curried1 = Currying((a: string, b: number, c: boolean) => true)
-const curried2 = Currying((a: string, b: number, c: boolean, d: boolean, e: boolean, f: string, g: boolean) => true)
+type Curryied<T> = T extends (...args: infer Args) => infer R
+  ? Args extends [infer F, ...infer L]
+    ? (...args: [F]) => Curryied<(...args: L) => R>
+    : R
+  : never;
+
+declare function Currying<T>(fn: T): Curryied<T>;
+
+const curried1 = Currying((a: string, b: number, c: boolean) => true);
+const curried2 = Currying(
+  (
+    a: string,
+    b: number,
+    c: boolean,
+    d: boolean,
+    e: boolean,
+    f: string,
+    g: boolean,
+  ) => true,
+);
 
 type cases = [
-  Expect<Equal<
-    typeof curried1, (a: string) => (b: number) => (c: boolean) => true
-  >>,
-  Expect<Equal<
-    typeof curried2, (a: string) => (b: number) => (c: boolean) => (d: boolean) => (e: boolean) => (f: string) => (g: boolean) => true
-  >>,
-]
+  Expect<
+    Equal<typeof curried1, (a: string) => (b: number) => (c: boolean) => true>
+  >,
+  Expect<
+    Equal<
+      typeof curried2,
+      (
+        a: string,
+      ) => (
+        b: number,
+      ) => (
+        c: boolean,
+      ) => (d: boolean) => (e: boolean) => (f: string) => (g: boolean) => true
+    >
+  >,
+];
