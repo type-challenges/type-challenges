d4575613b515df325c0ec6697ebd0fa06310cb44
wangj <wangj@chinawyny.com>
2022-07-04 15:17:11 +0800

    UnionToIntersection

--- questions/00055-hard-union-to-intersection/test-cases.ts
+++ questions/00055-hard-union-to-intersection/test-cases.ts
@@ -1,6 +1,17 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type UnionToIntersection<U> = (
+  U extends any ? (arg: U) => any : never
+) extends (arg: infer I) => void
+  ? I
+  : never;
 
 type cases = [
   Expect<Equal<UnionToIntersection<'foo' | 42 | true>, 'foo' & 42 & true>>,
-  Expect<Equal<UnionToIntersection<(() => 'foo') | ((i: 42) => true)>, (() => 'foo') & ((i: 42) => true)>>,
-]
+  Expect<
+    Equal<
+      UnionToIntersection<(() => 'foo') | ((i: 42) => true)>,
+      (() => 'foo') & ((i: 42) => true)
+    >
+  >,
+];
