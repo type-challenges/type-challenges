1d1913cbdb77a1026688313dc8592f6d42a6dce5
wangj <wangj@chinawyny.com>
2022-07-05 16:12:30 +0800

    get

--- questions/00223-hard-isany/test-cases.ts
+++ questions/00223-hard-isany/test-cases.ts
@@ -1,4 +1,12 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type IsAny<T> = (<K>() => K extends T ? 1 : 2) extends <K>() => K extends any
+  ? 1
+  : 2
+  ? true
+  : false;
+
+//type IsAny<T> = 0 extends (1 & T) ? true : false;
 
 type cases = [
   Expect<Equal<IsAny<any>, true>>,
@@ -7,4 +15,4 @@ type cases = [
   Expect<Equal<IsAny<unknown>, false>>,
   Expect<Equal<IsAny<never>, false>>,
   Expect<Equal<IsAny<string>, false>>,
-]
+];
--- questions/00270-hard-typed-get/test-cases.ts
+++ questions/00270-hard-typed-get/test-cases.ts
@@ -1,5 +1,15 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Splice<K extends string,R extends string[] = []> = K extends `${infer F}.${infer L}` ? Splice<L,[...R,F]> : [...R,K & string]
+
+
+type Get<T, K extends string,P extends string[] = Splice<K>> = 
+  P['length'] extends 1 
+  ? T[P[0] & keyof T] 
+  : P extends [string,...infer L extends string[]] 
+    ? Get<T[P[0] & keyof T], K , L>
+    : never
+
 type cases = [
   Expect<Equal<Get<Data, 'hello'>, 'world'>>,
   Expect<Equal<Get<Data, 'foo.bar.count'>, 6>>,
