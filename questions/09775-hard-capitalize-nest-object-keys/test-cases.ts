cd6ad1c45a96b6e48fb302bc8b5d0ec0915a60bf
antfu <antfu@users.noreply.github.com>
2022-07-05 00:03:45 +0000

    chore: update index

--- questions/09775-hard-capitalize-nest-object-keys/test-cases.ts
+++ questions/09775-hard-capitalize-nest-object-keys/test-cases.ts
@@ -2,17 +2,17 @@ import type { Equal, Expect } from '@type-challenges/utils'
 import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type foo = {
-    foo: string;
-    bars: [{ foo: string }];
-};
+  foo: string
+  bars: [{ foo: string }]
+}
 
-type Foo =  {
-    Foo: string;
-    Bars: [{
-        Foo: string;
-    }];
+type Foo = {
+  Foo: string
+  Bars: [{
+    Foo: string
+  }]
 }
 
 type cases = [
-  Expect<Equal<Foo, CapitalizeNestObjectKeys<foo>>>
+  Expect<Equal<Foo, CapitalizeNestObjectKeys<foo>>>,
 ]
