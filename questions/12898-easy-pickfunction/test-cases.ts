a296f2925eee1b6ce4ed95dab8425b186271f6bc
xk <52523698+XkSuperCool@users.noreply.github.com>
2022-07-12 00:58:03 +0000

    feat(question): add #12898 - PickFunction

---
+++ questions/12898-easy-pickfunction/test-cases.ts
@@ -0,0 +1,16 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface Todo {
+  title: string
+  description: string
+  foo(): string
+  bar(name: string): string
+}
+
+type cases = [
+  Expect<Equal<PickFunction<Todo>, {
+    foo(): string
+    bar(name: string): string
+  }>>
+]
