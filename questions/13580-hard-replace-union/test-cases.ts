36435c34ff148161a2a7a13d69a6bb3ac9433d7d
Konstantin Barabanov <19373212+crutch12@users.noreply.github.com>
2022-07-26 22:57:14 +0000

    feat(question): add #13580 - Replace Union

---
+++ questions/13580-hard-replace-union/test-cases.ts
@@ -0,0 +1,10 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  // string -> null
+  Expect<Equal<UnionReplace<number | string, [[string, null]]>, number | null>>,
+
+  // Date -> string; Function -> undefined
+  Expect<Equal<UnionReplace<Function | Date | object, [[Date, string], [Function, undefined]]>, undefined | string | object>>,
+]
