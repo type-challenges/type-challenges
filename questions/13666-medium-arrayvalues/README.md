31ff5792eef55da9db9490c265d6bc40eee71efd
Seb <68665394+sebmeister2077@users.noreply.github.com>
2022-07-28 18:53:20 +0000

    feat(question): add #13666 - ArrayValues

---
+++ questions/13666-medium-arrayvalues/README.md
@@ -0,0 +1,9 @@
+For example: 
+
+```
+type MyArr = ["hello","world",1,1]
+type T0 = ArrayValues<MyArr> // "hello" | "world" | 1;
+
+const arrVariable = ["hello","world",1]
+type T1 = ArrayValues<typeof arrVariable> // string | number;
+```
---
+++ questions/13666-medium-arrayvalues/test-cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+const arrayVariable = ["number",3]
+type cases = [
+  Expect<Equal<ArrayValues<["hi","there"]>, "hi" | "there">>,
+  Expect<Equal<ArrayValues<[]>, never>>,
+  Expect<Equal<ArrayValues<typeof arrayVariable>, string | number>>,
+]
