dfdc8d4e9be905438edb42c4a8ae2495fe4b85da
caseycameron <mcasey.cameron@gmail.com>
2022-08-19 13:42:11 -0700

    RED - 18

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,15 @@
+/*
+For given a tuple, create a generic `Length`, pick the length of the tuple
+
+For example:
+
+```ts
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type teslaLength = Length<tesla>  // expected 4
+type spaceXLength = Length<spaceX> // expected 5
+```
+*/
+
 type Length<T> = any
