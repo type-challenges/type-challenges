128829a2606ef2d3a821cc3eb1f9a74a19a87592
caseycameron <mcasey.cameron@gmail.com>
2022-08-19 11:21:26 -0700

    RED - 07

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,22 @@
+/*
+Implement the built-in Readonly<T> generic without using it.
+
+Constructs a type with all properties of T set to readonly, meaning the properties of the constructed type cannot be reassigned.
+
+For example:
+
+interface Todo {
+  title: string
+  description: string
+}
+
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar"
+}
+
+todo.title = "Hello" // Error: cannot reassign a readonly property
+todo.description = "barFoo" // Error: cannot reassign a readonly property
+*/
+
 type MyReadonly<T> = any
