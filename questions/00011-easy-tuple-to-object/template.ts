b63da3badf3d02bcdc2474e25baf9a5edd50ffaa
caseycameron <mcasey.cameron@gmail.com>
2022-08-19 11:29:31 -0700

    RED - 11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,11 @@
+/*
+Give an array, transform into an object type and the key/value must in the given array.
+
+For example:
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+*/
+
 type TupleToObject<T extends readonly any[]> = any
