a5c5cd1d715b3a1989fef346c7fa83074c6e1fb8
caseycameron <mcasey.cameron@gmail.com>
2022-08-19 11:58:43 -0700

    Green - 11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -8,4 +8,27 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 */
 
-type TupleToObject<T extends readonly any[]> = any
+// must specify string or number to meet error criteria. The input array T will contain strings or numbers
+// it needs to be readonly because casting the tuples as a const in the test file makes it immutable
+// for every item in the T array where T[number] refers to the index type
+// create a new object with that [index in T[number]] key and index as a value
+type TupleToObject<T extends readonly (string | number)[]> = { [index in T[number]]: index }
+
+/*
+TEST FILE --
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const tupleMix = [1, '2', 3, '4'] as const
+
+type cases = [
+  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
+]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+*/
