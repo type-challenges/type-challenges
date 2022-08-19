a64b45aaf6a8eeafcb11dac84001eeb8614445d3
caseycameron <mcasey.cameron@gmail.com>
2022-08-19 11:59:52 -0700

    RED - 14

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,13 @@
+/*
+Implement a generic First<T> that takes an Array T and returns it's first element's type.
+
+For example:
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
+*/
+
 type First<T extends any[]> = any
