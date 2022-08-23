1b1a4ef79723b3b7789ce9f2ce14c0b281df8d52
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 08:54:16 -0700

    Green - 189

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,21 @@
-type MyAwaited<T> = any
+/*
+If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type?
+
+For example: if we have Promise<ExampleType> how to get ExampleType?
+
+type ExampleType = Promise<string>
+
+type Result = MyAwaited<ExampleType> // string
+*/
+
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer U>
+  ? U extends Promise<unknown>
+    ? MyAwaited<U>
+    : U
+  : never
+
+// First (or last), if nothing exists assign never - should never happen - it must be a Promise
+// take some unknown types and create T by extending from the generic Promise and inferring the Promise types
+// if something exists U will extend the inferred types from the Promise. This Promise is a wrapper
+// if the inferred types are also a Promise, assign MyAwaited to U (unwrap those inferred inner types)
+// otherwise assign them U
