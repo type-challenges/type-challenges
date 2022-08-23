ae0747affea62870a8a14a0bc44027f589fb0813
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 08:16:13 -0700

    Green

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,4 @@
-type MyExclude<T, U> = any
+// Exclude from T those types that are assignable to U
+
+// this is the literal definition of the Exclude type from TypeScript docs
+type MyExclude<T, U> = T extends U ? never : T
