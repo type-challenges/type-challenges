fad7a1aab91e4375ea3c1ee19adcfbdb510200a5
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 13:22:50 -0700

    Green - 002

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,12 @@
-type MyReturnType<T> = any
+/*
+const fn = (v: boolean) => {
+  if (v)
+    return 1
+  else
+    return 2
+}
+
+type a = MyReturnType<typeof fn> // should be "1 | 2"
+*/
+
+type MyReturnType<T> = T extends (...args: any) => infer argType ? argType : 2
