c6923b0743739d60a1f8d97617eaf65ae25977b2
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 09:34:46 -0700

    Green 533

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,11 @@
-type Concat<T, U> = any
+/*
+Implement the JavaScript Array.concat function in the type system.
+A type takes the two arguments.
+The output should be a new array that includes inputs in ltr order
+
+For example:
+
+type Result = Concat<[1], [2]> // expected to be [1, 2]
+*/
+
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
