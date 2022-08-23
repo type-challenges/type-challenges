2c925784035b0073e4f87da8743940b4df03fab9
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 10:21:49 -0700

    Green - 3060

--- questions/03060-easy-unshift/template.ts
+++ questions/03060-easy-unshift/template.ts
@@ -1,1 +1,8 @@
-type Unshift<T, U> = any
+/* Implement the type version of Array.unshift
+
+For example:
+
+type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+*/
+
+type Unshift<T extends unknown[], U> = [U, ...T]
