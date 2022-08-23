e160219aab18e88d0d88b472f683904c51b76ed4
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 10:20:28 -0700

    Green 3057

--- questions/03057-easy-push/template.ts
+++ questions/03057-easy-push/template.ts
@@ -1,1 +1,10 @@
-type Push<T, U> = any
+/*
+Implement the generic version of Array.push
+
+For example:
+
+type Result = Push<[1, 2], '3'> // [1, 2, '3']
+
+*/
+
+type Push<T extends unknown[], U> = [...T, U]
