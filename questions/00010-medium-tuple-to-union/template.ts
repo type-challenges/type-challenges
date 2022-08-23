a006539cab7c6cb7d1c2019a6776b1304bbb0f81
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 15:54:46 -0700

    Green - 010

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,13 @@
-type TupleToUnion<T> = any
+/*
+Implement a generic TupleToUnion<T> which covers the values of a tuple to its values union.
+
+For example
+
+type Arr = ['1', '2', '3']
+
+type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+*/
+
+type TupleToUnion<T extends unknown[]> = T[number]
+// extend whatever array type you get
+// assign each element in the array to its own type in a union type
