d2fa9361b53c8f7a4813ccd541d468b4272c0670
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 15:38:57 -0700

    Green 009

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,38 @@
-type DeepReadonly<T> = any
+/*
+Implement a generic DeepReadonly<T> which make every parameter of an object -
+and its sub-objects recursively - readonly.
+
+You can assume that we are only dealing with Objects in this challenge.
+Arrays, Functions, Classes and so on do not need to be taken into consideration.
+However, you can still challenge yourself by covering as many different cases as possible.
+
+For example:
+type X = {
+  x: {
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+
+type Expected = {
+  readonly x: {
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey'
+}
+
+type Todo = DeepReadonly<X> // should be same as `Expected`
+*/
+
+type List = string | number | boolean | undefined | null | Function
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends List ? T[K] : DeepReadonly<T[K]>
+}
+
+// create a list of the types we want to work with
+// make each key in T readonly
+// extend each of those keys from List, and if true return the key into this object
+// otherwise recursively call DeepReadonly and assign the key
