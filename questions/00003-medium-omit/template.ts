ea3465563780b4ebe771bc3d5d8f6a2178df4395
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 13:38:25 -0700

    Green 003

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,25 @@
-type MyOmit<T, K> = any
+/*
+Implement the built-in Omit<T, K> generic without using it.
+
+Constructs a type by picking all properties from T and then removing K
+
+For example
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+const todo: TodoPreview = {
+  completed: false,
+}
+*/
+
+type MyOmit<T, K extends keyof T> = { [key in Exclude<keyof T, K>]: T[key] }
+// take in T
+// add T's keys to K
+// for each key in T, filter out any that match K
+// return a generic object with the non-filtered keys
