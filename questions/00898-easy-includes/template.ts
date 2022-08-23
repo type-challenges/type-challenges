ae1af5089a468d553b9b7a9b987e346b11fa2508
caseycameron <mcasey.cameron@gmail.com>
2022-08-23 10:13:14 -0700

    Issue - 898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,13 @@
-type Includes<T extends readonly any[], U> = any
+/*
+Implement the JavaScript Array.includes function in the type system.
+A type takes the two arguments.
+The output should be a boolean true or false.
+
+For example:
+
+type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+
+*/
+
+type Includes<T extends readonly unknown[], U> = U extends T[number] ? true : false
+// this solution makes sense to me but the test's rigidity fails some cases where it should not
