a2d2f3a0a16d93548e2ecc4278462ad5d3c1aa27
caseycameron <mcasey.cameron@gmail.com>
2022-08-24 13:23:23 -0700

    Green 110

--- questions/00110-medium-capitalize/template.ts
+++ questions/00110-medium-capitalize/template.ts
@@ -1,1 +1,13 @@
-type MyCapitalize<S extends string> = any
+/*
+Implement Capitalize<T> which converts the first letter of a string to uppercase and leave the rest as-is.
+
+For example
+
+type capitalized = Capitalize<'hello world'> // expected to be 'Hello world'
+*/
+
+type MyCapitalize<S extends string> = S extends `${infer F}${infer R}` ? `${Uppercase<F>}${R}` : S
+
+// Grab the 1st char and then the rest
+// if true Uppercase 1st
+// If not true just return the char
