daae2a30e6601a30311133a1135633b8d1a89955
caseycameron <mcasey.cameron@gmail.com>
2022-08-24 14:37:04 -0700

    Green 191

--- questions/00191-medium-append-argument/template.ts
+++ questions/00191-medium-append-argument/template.ts
@@ -1,1 +1,18 @@
-type AppendArgument<Fn, A> = any
+/*
+For given function type Fn, and any type A, create a generic type which will take Fn as the first argument, A as the second,
+and will produce function type G which will be the same as Fn but with appended argument A as a last one.
+
+For example,
+type Fn = (a: number, b: string) => number
+
+type Result = AppendArgument<Fn, boolean>
+// expected be (a: number, b: string, x: boolean) => number
+*/
+type AppendArgument<Fn, A> = Fn extends (...args: infer T) => infer R
+  ? (...args: [...T, A]) => R
+  : never
+
+// Fn extends a function with inferred arg types and inferred return
+// If true, spread the args which are an array of the original T args plus the new A arg
+// Return the original inferred R
+// Else - it doesn't matter. It returns nothing.
