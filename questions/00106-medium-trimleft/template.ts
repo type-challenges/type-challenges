24dbbe3afc9c8a4924d678b073ae9fed95968139
caseycameron <mcasey.cameron@gmail.com>
2022-08-24 10:33:53 -0700

    Green 106

--- questions/00106-medium-trimleft/template.ts
+++ questions/00106-medium-trimleft/template.ts
@@ -1,1 +1,17 @@
-type TrimLeft<S extends string> = any
+/*
+Implement TrimLeft<T> which takes an exact string type and returns a new string with the whitespace beginning removed.
+
+For example
+
+type trimmed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
+*/
+type Whitespace = ' ' | '\n' | '\t'
+
+type TrimLeft<S extends string> = S extends `${Whitespace}${infer R}` ? TrimLeft<R> : S
+
+/*
+`S` extends the characters in the string.
+`Whitespace` will be one of those three union types.
+Recursively run the characters through `TrimLeft` to remove each matching `Whitespace`
+Once `false`, return the `string`.
+*/
