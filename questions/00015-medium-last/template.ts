4b7c80cdd9da6bc7595e5b49aa34eec464128388
caseycameron <mcasey.cameron@gmail.com>
2022-08-24 08:39:13 -0700

    Green - 015

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,14 @@
-type Last<T extends any[]> = any
+/*
+Implement a generic Last<T> that takes an Array T and returns its last element.
+
+For example
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
+
+*/
+
+type Last<T extends unknown[]> = T extends [...any, infer Last] ? Last : never
