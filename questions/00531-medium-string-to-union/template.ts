df0af3a2f089a89af59e759c274f034b7820aa5b
caseycameron <mcasey.cameron@gmail.com>
2022-08-25 08:46:51 -0700

    Green 531

--- questions/00531-medium-string-to-union/template.ts
+++ questions/00531-medium-string-to-union/template.ts
@@ -1,1 +1,16 @@
-type StringToUnion<T extends string> = any
+/*
+Implement the String to Union type. Type take string argument. The output should be a union of input letters
+
+For example
+
+type Test = '123';
+type Result = StringToUnion<Test>; // expected to be "1" | "2" | "3"
+*/
+
+type StringToUnion<T extends string> = T extends `${infer Top}${infer Tail}`
+  ? Top | StringToUnion<Tail>
+  : never
+
+// T will extend the first character and the following characters
+// If True, output Top | and recursively run the Tail through StringToUnion
+// Otherwise, don't return anything because it's already finished. 
