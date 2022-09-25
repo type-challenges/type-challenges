2a3794139da057e57f9f428dbb5cf4258b140bd6
elianli <cjwddz@gmail.com>
2022-09-25 15:32:30 +0800

    Update test-cases.ts
    add test case.

--- questions/00016-medium-pop/test-cases.ts
+++ questions/00016-medium-pop/test-cases.ts
@@ -3,4 +3,5 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
 ]
