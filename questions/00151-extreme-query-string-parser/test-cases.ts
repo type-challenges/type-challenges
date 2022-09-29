3ba6cec9f16dd2a0b524531c04cbfd32f06dbad0
Humandetail <39545747+humandetail@users.noreply.github.com>
2022-09-30 00:34:23 +0800

    feat: add case to 00151-extreme-query-string-parser (#16567)

--- questions/00151-extreme-query-string-parser/test-cases.ts
+++ questions/00151-extreme-query-string-parser/test-cases.ts
@@ -11,4 +11,5 @@ type cases = [
   Expect<Equal<ParseQueryString<'k1=v1&k2=v2&k1=v2'>, { k1: ['v1', 'v2']; k2: 'v2' }>>,
   Expect<Equal<ParseQueryString<'k1=v1&k2'>, { k1: 'v1'; k2: true }>>,
   Expect<Equal<ParseQueryString<'k1=v1&k1=v1'>, { k1: 'v1' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2=v2&k1=v2&k1=v3'>, { k1: ['v1', 'v2', 'v3']; k2: 'v2' }>>,
 ]
