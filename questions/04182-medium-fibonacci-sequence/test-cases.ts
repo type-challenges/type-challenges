b8989ed8ddfc9957e6cc300205f11f1e9266e5db
laggage <laggage-la@outlook.com>
2022-10-02 16:15:25 +0800

    chore: add cases for 04182-medium-fibonacci-sequence

--- questions/04182-medium-fibonacci-sequence/test-cases.ts
+++ questions/04182-medium-fibonacci-sequence/test-cases.ts
@@ -1,6 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
+  Expect<Equal<Fibonacci<1>, 1>>,
+  Expect<Equal<Fibonacci<2>, 1>>,
   Expect<Equal<Fibonacci<3>, 2>>,
   Expect<Equal<Fibonacci<8>, 21>>,
 ]
