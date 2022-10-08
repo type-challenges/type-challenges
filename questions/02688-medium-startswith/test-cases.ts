20c2f03ae1460aced4c60dd18d81f0f6e421beb2
Kkk <71477878+Stan-BK@users.noreply.github.com>
2022-10-08 20:38:59 +0800

    Update test-cases.ts (#17195)

--- questions/02688-medium-startswith/test-cases.ts
+++ questions/02688-medium-startswith/test-cases.ts
@@ -3,6 +3,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
   Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abc'>, true>>,
   Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
   Expect<Equal<StartsWith<'abc', ''>, true>>,
   Expect<Equal<StartsWith<'abc', ' '>, false>>,
