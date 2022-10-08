f85a42ee2e290cb2b89698f80bfafb065979ac95
elianli <cjwddz@gmail.com>
2022-10-08 12:36:18 +0800

    opt: add test cases for 03062-medium-shift
    add critical condition test cases for 03062-medium-shift.

--- questions/03062-medium-shift/test-cases.ts
+++ questions/03062-medium-shift/test-cases.ts
@@ -1,6 +1,10 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
+  // @ts-expect-error
+  Shift<unknown>,
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
   Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
   Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
 ]
