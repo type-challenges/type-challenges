64b470f02832cad42ce7c41bfcbbff068d3a6d1e
cutefcc <63641648+cutefcc@users.noreply.github.com>
2022-10-25 05:28:35 +0000

    feat(question): add #17964 - Reverse tuple

---
+++ questions/17964-easy-reverse-tuple/README.md
@@ -0,0 +1,11 @@
+Reverse tuple, behave like：`Array.prototype.reverse()`
+
+like：
+
+```ts
+type tech = ["html", "css", "typescript", "node"]
+type num = [1, 2, 3, 4, 5]
+
+type ReverseTech = Reverse<tech> // expected ['node', 'typescript', 'css', 'html']
+type ReverseNum = Reverse<num> // expected [5, 4, 3, 2, 1]
+```
---
+++ questions/17964-easy-reverse-tuple/test-cases.ts
@@ -0,0 +1,7 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<Reverse<[1, 2, 3, 4, 5]>, [5, 4, 3, 2, 1]>>,
+    Expect<Equal<Reverse<[null, undefined, string, number]>, [number, string, undefined, null]>>,
+    Expect<Equal<Reverse<[string, 1, number, "a", []]>, [[], "a", number, 1, string]>>,
+];
