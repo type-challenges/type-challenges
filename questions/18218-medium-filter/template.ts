7b01c68e8e6d911b0e5a9252155cde269310865a
Muhun Kim <5278201+x86chi@users.noreply.github.com>
2022-10-29 04:39:20 +0000

    feat(question): add #18218 - Filter

---
+++ questions/18218-medium-filter/template.ts
@@ -0,0 +1,5 @@
+type a = Filter<[0, 1, 2], 2> // expected to be [2]
+type b = Filter<[0, 1, 2], 0 | 1> // expected to be [0, 1]
+
+type Falsy = false | 0 | '' | null | undefined
+type c = Filter<[0, 1, 2], Falsy> // expected to be [0]
