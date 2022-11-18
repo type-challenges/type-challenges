3099d1f2dd099446b14a53da978f3ca75e2d56c9
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-11-18 18:24:13 +0800

    feat(03192-medium-reverse)-add-error-cases (#19326)

--- questions/03192-medium-reverse/test-cases.ts
+++ questions/03192-medium-reverse/test-cases.ts
@@ -5,3 +5,10 @@ type cases = [
   Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
   Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
 ]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>,
+]
