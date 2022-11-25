4715e3745519218c697585fc1c38735a6b18fe86
Takahiro Kato <30111741+doz13189@users.noreply.github.com>
2022-11-25 09:43:37 +0000

    feat(question): add #19749 - IsEqual

---
+++ questions/19749-medium-isequal/test-cases.ts
@@ -0,0 +1,10 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsEqual<number, string>, false>>,
+  Expect<Equal<IsEqual<1, 1>, true>>,
+  Expect<Equal<IsEqual<any, 1>, false>>,
+  Expect<Equal<IsEqual<1 | 2, 1>, false>>,
+  Expect<Equal<IsEqual<any, never>, false>>,
+  Expect<Equal<IsEqual<[any], [number]>, false>>,
+]
