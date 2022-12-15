5c5bc120930ab2b666cb1e8a084a64c829317f86
Stefan Kern <2032760+StefanKern@users.noreply.github.com>
2022-12-15 15:20:47 +0000

    feat(question): add #20744 - Overwrtie type with type of child

---
+++ questions/20744-easy-overwrtie-type-with-type-of-child/README.md
@@ -0,0 +1,66 @@
+Assuming you have and object of type
+```ts
+{
+   "id": number,
+   "foo": {
+        "id": number,
+        "data": {
+            "id": number,
+            "prop1": any,
+            "prop2": any
+        }
+   }
+}
+```
+
+and we want to remove the nesting of the data to get a type
+```ts
+{
+   "id": number,
+   "foo": {
+        "id": number,
+        "prop1": any,
+        "prop2": any
+   }
+}
+```
+
+Should also work with deeper levels of nesting e.g.
+
+```ts
+interface DeepModel {
+  'id': number,
+  'foo': {
+    'id': number,
+    'data': {
+      'id': number,
+      'prop1': any,
+      'prop2': any,
+      'bar': {
+        'id': number,
+        'data': {
+          'id': number,
+          'prop1': any,
+          'prop2': any,
+        }
+      }
+    }
+  }
+}
+```
+should be reduced to
+```ts
+interface ReducedDeepModel {
+  'id': number,
+  'foo': {
+    'id': number,
+    'prop1': any,
+    'prop2': any,
+    'bar': {
+      'id': number,
+      'prop1': any,
+      'prop2': any,
+    }
+  }
+}
+```
---
+++ questions/20744-easy-overwrtie-type-with-type-of-child/test-cases.ts
@@ -0,0 +1,61 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Model {
+   "id": number,
+   "extension": {
+        "id": number,
+        "data": {
+            "id": number,
+            "prop1": any,
+            "prop2": any
+        }
+   }
+}
+
+type ReducedModel = {
+   "id": number,
+   "extension": {
+        "id": number,
+        "prop1": any,
+        "prop2": any
+   }
+}
+
+interface DeepModel {
+  'id': number,
+  'foo': {
+    'id': number,
+    'data': {
+      'id': number,
+      'prop1': any,
+      'prop2': any,
+      'bar': {
+        'id': number,
+        'data': {
+          'id': number,
+          'prop1': any,
+          'prop2': any,
+        }
+      }
+    }
+  }
+}
+
+interface ReducedDeepModel {
+  'id': number,
+  'foo': {
+    'id': number,
+    'prop1': any,
+    'prop2': any,
+    'bar': {
+      'id': number,
+      'prop1': any,
+      'prop2': any,
+    }
+  }
+}
+
+type cases = [
+  Expect<Equal<ObjectFromEntries<ReducedModel>, Model>>,
+  Expect<Equal<ObjectFromEntries<DeepModel>, ReducedDeepModel>>,
+]
