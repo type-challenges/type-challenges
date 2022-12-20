3047da34054e4fb6973181afd6c4e416623f4ade
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2022-12-20 08:25:43 +0000

    feat(question): add #20901 - key_type from props

---
+++ questions/20901-hard-key_type-from-props/template.ts
@@ -0,0 +1,1 @@
+type KeyTypeFromProps<T> = any
---
+++ questions/20901-hard-key_type-from-props/test-cases.ts
@@ -0,0 +1,37 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type obj = {
+  [x: string]: (
+    | {
+      component: 'AInput'
+      value: string
+    }
+    | {
+      component: 'ASelect'
+      value: string
+    }
+    | {
+      component: 'ARangePicker'
+      value: [number, number]
+    }
+  ) & {
+    label?: string
+  }
+}
+
+type res = {
+  AInput: string;
+  ASelect: string;
+  ARangePicker: [number, number];
+}
+
+type cases = [
+  Expect<Equal<KeyTypeFromProps<obj, 'component', 'value'>, res>>,
+
+  // @ts-expect-error
+  KeyTypeFromProps<{ x: 1 }, '', ''>,
+  // @ts-expect-error
+  KeyTypeFromProps<obj, 'fake', 'fake'>,
+  // @ts-expect-error
+  KeyTypeFromProps<[], '', ''>
+]
