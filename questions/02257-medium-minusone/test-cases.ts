fa54f51c4b294f2050a6d55f4df27cf8da5db0a0
gaac510 <74474003+gaac510@users.noreply.github.com>
2022-12-24 06:30:21 +1100

    02257-medium-minusone: Add a test case with an absurdly large number (#21034)

--- questions/02257-medium-minusone/test-cases.ts
+++ questions/02257-medium-minusone/test-cases.ts
@@ -7,4 +7,5 @@ type cases = [
   Expect<Equal<MinusOne<100>, 99>>,
   Expect<Equal<MinusOne<1101>, 1100>>,
   Expect<Equal<MinusOne<0>, -1>>,
+  Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
 ]
