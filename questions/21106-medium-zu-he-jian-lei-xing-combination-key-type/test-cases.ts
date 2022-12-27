c3334a0fee5c8186c203c805c639abcad919ae67
Nauxscript <13010589+Nauxscript@users.noreply.github.com>
2022-12-27 04:00:37 +0000

    feat(question): add #21106 - 组合键类型 Combination key type

---
+++ questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/test-cases.ts
@@ -0,0 +1,7 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type CaseTypeOne = "cmd ctrl" | "cmd opt" | "cmd fn" | "ctrl opt" | "ctrl fn" | "opt fn"
+
+type cases = [
+  Expect<Equal<Combs, CaseTypeOne>>
+]
