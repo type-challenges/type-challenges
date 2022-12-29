b5b83f095c17bba9777d576f7d77bbe95c501843
gaac510 <74474003+gaac510@users.noreply.github.com>
2022-12-29 12:54:50 +1100

    Test the maximum allowed tuple size i.e. 9999

--- questions/07544-medium-construct-tuple/test-cases.ts
+++ questions/07544-medium-construct-tuple/test-cases.ts
@@ -3,7 +3,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<ConstructTuple<0>, []>>,
   Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
-  Expect<Equal<ConstructTuple<999>['length'], 999>>,
+  Expect<Equal<ConstructTuple<9999>['length'], 9999>>,
   // @ts-expect-error
-  Expect<Equal<ConstructTuple<1000>['length'], 1000>>,
+  Expect<Equal<ConstructTuple<10000>['length'], 10000>>,
 ]
