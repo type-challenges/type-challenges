c5f68d1e2388d12af0e3c5e7b5dc6b74f23a2c63
cheremuhinikita <nikitadf68@gmail.com>
2023-01-09 00:04:54 +0300

    complete "easy Length of Tuple"

--- playground/00018-easy-length-of-tuple.ts
+++ playground/00018-easy-length-of-tuple.ts
@@ -22,7 +22,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Length<T> = any
+type Length<T extends readonly unknown[]> = T['length']
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
