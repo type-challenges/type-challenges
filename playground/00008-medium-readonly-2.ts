2081e4a2254e7b64278101c2402c9e05a3609122
cheremuhinikita <nikitadf68@gmail.com>
2023-01-09 23:42:02 +0300

    complete "medium Readonly 2"

--- playground/00008-medium-readonly-2.ts
+++ playground/00008-medium-readonly-2.ts
@@ -34,7 +34,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P];
+}
 
 /* _____________ Test Cases _____________ */
 import type { Alike, Expect } from '@type-challenges/utils'
