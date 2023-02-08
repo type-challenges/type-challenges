f967e89635d6e5f0222af759c219af1903b17548
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-08 15:40:56 -0500

    answer added

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,8 @@
-type MyAwaited<T> = any
+type ExampleType = Promise<string | number>
+type Result = MyAwaited<ExampleType> // string
+
+
+// infers type from deeply nested promise
+type MyAwaited<T extends Promise<unknown>> = 
+T extends Promise<infer R> ? (R extends Promise<unknown> ? MyAwaited<R> : R) : never;
+
