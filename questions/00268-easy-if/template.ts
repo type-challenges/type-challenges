9db7c1fe0c10305f7d2e8c16cf89b055758325ce
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-08 15:44:54 -0500

    answer added

--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,4 @@
-type If<C, T, F> = any
+type A = If<true, 'a', 'b'>  // expected to be 'a'
+type B = If<false, 'a', 'b'> // expected to be 'b'
+
+type If<C extends boolean, T, F> = C extends true ? T : F
