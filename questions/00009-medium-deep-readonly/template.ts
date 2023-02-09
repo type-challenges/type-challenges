5c28def3b19c5965c891cbbc8c1420d43dcb9085
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-09 09:57:05 -0500

    answer added

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,21 @@
-type DeepReadonly<T> = any
+export type DeepReadonly<T> = {
+    readonly [K in keyof T] : T[K] extends Function | string | number | symbol ? T[K] : DeepReadonly<T[K]>
+}
+
+type X = { 
+    x: { 
+      a: 1
+      b: 'hi'
+    }
+    y: 'hey'
+  }
+  
+  type Expected = { 
+    readonly x: { 
+      readonly a: 1
+      readonly b: 'hi'
+    }
+    readonly y: 'hey' 
+  }
+  
+  type Todo = DeepReadonly<X> 
