c3190e907669a3a1152b18a68d654fcfdde6932f
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-09 14:49:48 -0500

    answer added

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,4 @@
-type Last<T extends any[]> = any
+export type Last<T extends unknown[]> = T extends [...unknown[], infer tail] ? tail : never
+
+type arr = ['a', 'b', 'c']
+type tail = Last<arr> // return 'c'
