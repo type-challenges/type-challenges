6f24978b52f38b8c94e9d471bf7a771fa2a6cacf
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-09 10:02:26 -0500

    answer added

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,5 @@
-type TupleToUnion<T> = any
+export type TupleToUnion<T extends unknown[]> = T[number]
+
+type Arr = ['1', '2', '3']
+type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+
--- questions/00010-medium-tuple-to-union/test-cases.ts
+++ questions/00010-medium-tuple-to-union/test-cases.ts
@@ -1,4 +1,5 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '../../utils'
+import type { TupleToUnion } from './template'
 
 type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
