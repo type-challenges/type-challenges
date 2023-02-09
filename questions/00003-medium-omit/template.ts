c5adfe8a96498168e706bc541c8b1859e51177c2
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-08 22:41:55 -0500

    answer added

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,11 @@
-type MyOmit<T, K> = any
+export type MyOmit<T, K extends keyof T> = {
+    [R in keyof T as R extends K ? never : R]: T[R]
+}
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
