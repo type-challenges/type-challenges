87788bea1b870b476702d7ffecd8a9747f8f4ca6
Kishan Bhimani <kishanbhimani@Kishans-MacBook-Pro-2.local>
2023-02-16 20:08:49 -0500

    answer added

--- questions/00106-medium-trimleft/template.ts
+++ questions/00106-medium-trimleft/template.ts
@@ -1,5 +1,5 @@
 
-type Space = " " | "\n" | "\t"
+export type Space = " " | "\n" | "\t"
 export type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R>: S;
 
 type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,6 @@
-type Trim<S extends string> = any
+import { Space, TrimLeft } from "../00106-medium-trimleft/template"
+
+export type TrimRight<S extends string> = S extends `${infer R}${Space}` ? TrimRight<R> : S
+export type Trim<S extends string> = TrimLeft<S> extends `${infer R}${Space}` ? TrimRight<R> : TrimLeft<S>
+
+type trimmed = Trim<'  Hello World'> // expected to be 'Hello World'
