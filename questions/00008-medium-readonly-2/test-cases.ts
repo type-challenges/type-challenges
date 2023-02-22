73d675bd3ac2d789e45b3177384ced580e2790ed
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2023-02-22 12:06:30 +0100

    enhance: revert previous commit, add new test case

--- questions/00008-medium-readonly-2/test-cases.ts
+++ questions/00008-medium-readonly-2/test-cases.ts
@@ -3,7 +3,8 @@ import type { Alike, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
   Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
 ]
 
 // @ts-expect-error
