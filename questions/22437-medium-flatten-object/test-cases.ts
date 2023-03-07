2cccf437c858273612be48ab4ac66ce2bcf8a6d9
dh <61490799+yue1123@users.noreply.github.com>
2023-03-07 17:31:45 +0800

    feat: update delimiter to `.` and add test case

--- questions/22437-medium-flatten-object/test-cases.ts
+++ questions/22437-medium-flatten-object/test-cases.ts
@@ -1,7 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
-
+type FlattenObject = any
 
 const simple = {
   a: {}
@@ -16,8 +16,24 @@ const deep = {
   }
 }
 
+const duplicate = {
+  "a.b.c": true,
+  a: {
+    b: {
+      c: 123,
+      d: '123'
+    }
+  }
+}
 
 type cases = [
   Expect<Equal<FlattenObject<typeof simple>, { 'a': {} }>>,
-  Expect<Equal<FlattenObject<typeof deep>, { 'a/b/c': number, 'a/b/d': string }>>
+  Expect<Equal<FlattenObject<typeof deep>, { 'a.b.c': number, 'a.b.d': string }>>,
+  Expect<Equal<FlattenObject<typeof duplicate>, {
+    'a.b.c': boolean
+  }
+    | {
+      'a.b.c': number
+      'a.b.d': string
+    }>>
 ]
