bc0dc0e9923259e63b948c5408fc86ce6b7c007d
antfu <antfu@users.noreply.github.com>
2023-03-16 00:04:09 +0000

    chore: update index

--- questions/05140-medium-trunc/test-cases.ts
+++ questions/05140-medium-trunc/test-cases.ts
@@ -2,7 +2,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<Trunc<0.1>, '0'>>,
-  Expect<Equal<Trunc<.2>, '0'>>,
+  Expect<Equal<Trunc<0.2>, '0'>>,
   Expect<Equal<Trunc<1.234>, '1'>>,
   Expect<Equal<Trunc<12.345>, '12'>>,
   Expect<Equal<Trunc<-5.1>, '-5'>>,
