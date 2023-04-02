b9fe99ed6d4f5242d04bac4d9705ddaf6af8d932
andrew jarrett <15133992+ahrjarrett@users.noreply.github.com>
2023-04-02 10:58:28 +0000

    feat(question): add #25747 - IsNegativeNumber

---
+++ questions/25747-hard-isnegativenumber/test-cases.ts
@@ -0,0 +1,13 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsNegativeNumber<0>, false>>,
+  Expect<Equal<IsNegativeNumber<number>, never>>,
+  Expect<Equal<IsNegativeNumber<-1 | -2>, never>>,
+  Expect<Equal<IsNegativeNumber<-1>, true>>,
+  Expect<Equal<IsNegativeNumber<-1.9>, true>>,
+  Expect<Equal<IsNegativeNumber<-100_000_000>, true>>,
+  Expect<Equal<IsNegativeNumber<1>, false>>,
+  Expect<Equal<IsNegativeNumber<1.9>, false>>,
+  Expect<Equal<IsNegativeNumber<100_000_000>, false>>,
+]
