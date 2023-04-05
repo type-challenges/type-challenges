670ae1ea67fcef6b1e553f82aae208c9c4a07789
정원 <jwha94@gmail.com>
2023-04-05 17:40:28 +0900

    00268-easy-if (#7)

--- playground/00268-easy-if.ts
+++ playground/00268-easy-if.ts
@@ -19,18 +19,18 @@
 
 /* _____________ Your Code Here _____________ */
 
-type If<C, T, F> = any
+type If<C extends boolean, T, F> = C extends true ? T : F
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
 type cases = [
-  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
-  Expect<Equal<If<false, 'a', 2>, 2>>,
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
 ]
 
 // @ts-expect-error
-type error = If<null, 'a', 'b'>
+type error = If<null, "a", "b">
 
 /* _____________ Further Steps _____________ */
 /*
