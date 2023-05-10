def420df1c9002719f936dc2817a4783a232add4
antfu <antfu@users.noreply.github.com>
2023-05-10 00:03:37 +0000

    chore: update index

--- questions/27133-medium-square/test-cases.ts
+++ questions/27133-medium-square/test-cases.ts
@@ -11,5 +11,5 @@ type cases = [
   Expect<Equal<Square<-2>, 4>>,
   Expect<Equal<Square<-5>, 25>>,
   Expect<Equal<Square<-31>, 961>>,
-  Expect<Equal<Square<-50>, 2500>>
-];
+  Expect<Equal<Square<-50>, 2500>>,
+]
