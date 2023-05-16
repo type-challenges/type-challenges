b4a78ba774369c436f99c24ac131e80e52fdf757
Jason Zhang <xzha4350@gmail.com>
2023-05-16 19:58:52 +0930

    533 - add more test cases (#27259)

--- questions/00533-easy-concat/test-cases.ts
+++ questions/00533-easy-concat/test-cases.ts
@@ -1,8 +1,14 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+const tuple = [1] as const
+
 type cases = [
   Expect<Equal<Concat<[], []>, []>>,
   Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
 ]
+
+// @ts-expect-error
+type error = Concat<null, undefined>
