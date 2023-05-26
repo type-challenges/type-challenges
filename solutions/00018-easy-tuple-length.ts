81c2d9764a99f888f7d8ec1cd84221a1e56a8662
Davydov_Konstantin <k.davydov.dev@gmail.com>
2023-05-26 18:37:47 +0400

    00018-easy-pick

---
+++ solutions/00018-easy-tuple-length.ts
@@ -0,0 +1,27 @@
+/*
+For given a tuple, you need create a generic Length, pick the length of the tuple
+
+For example:
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type teslaLength = Length<tesla>  // expected 4
+type spaceXLength = Length<spaceX> // expected 5
+*/
+
+import { Equal, Expect } from "../utils"
+
+type Length<T extends readonly string[]> = T['length'];
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
