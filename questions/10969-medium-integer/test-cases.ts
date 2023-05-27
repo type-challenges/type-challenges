1a618e90b24e59f09c2ffe762eff01496bfbe605
jazelly <xzha4350@gmail.com>
2023-05-27 21:43:17 +0930

    test: add more test cases to 10969-medium-integer

--- questions/10969-medium-integer/test-cases.ts
+++ questions/10969-medium-integer/test-cases.ts
@@ -9,6 +9,9 @@ type cases1 = [
   Expect<Equal<Integer<1.1>, never>>,
   Expect<Equal<Integer<1.0>, 1>>,
   Expect<Equal<Integer<1.000000000>, 1>>,
+  Expect<Equal<Integer<0.5>, never>>,
+  Expect<Equal<Integer<28.00>, 28>>,
+  Expect<Equal<Integer<28.101>, never>>,
   Expect<Equal<Integer<typeof x>, never>>,
   Expect<Equal<Integer<typeof y>, 1>>,
 ]
