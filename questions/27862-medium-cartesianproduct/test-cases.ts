940392bb067e0dcd923d0d5d0203f679620c7ccf
jazelly <28685065+jazelly@users.noreply.github.com>
2023-05-31 11:42:49 +0000

    feat(question): add #27862 - CartesianProduct

---
+++ questions/27862-medium-cartesianproduct/test-cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CartesianProduct<1 | 2, 'a' | 'b'>, [2, 'a'] | [1, 'a'] | [2, 'b'] | [1, 'b']>>,
+  Expect<Equal<CartesianProduct<1 | 2 | 3, 'a' | 'b' | 'c' >, [2, 'a'] | [1, 'a'] | [3, 'a'] | [2, 'b'] | [1, 'b'] | [3, 'b'] | [2, 'c'] | [1, 'c'] | [3, 'c']>>,
+  Expect<Equal<CartesianProduct<1 | 2, 'a' | never>, [2, 'a'] | [1, 'a'] >>,
+  Expect<Equal<CartesianProduct<'a', Function | string>, ['a', Function] | ['a', string]>>,
+]
