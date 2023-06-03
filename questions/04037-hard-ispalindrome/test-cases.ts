68b280d73d54baf220cab5f555580fd6de714d38
jazelly <xzha4350@gmail.com>
2023-06-03 16:52:11 +0930

    test: add essential test cases to 4037-IsPalindrome (#27911)

--- questions/04037-hard-ispalindrome/test-cases.ts
+++ questions/04037-hard-ispalindrome/test-cases.ts
@@ -4,7 +4,9 @@ type cases = [
   Expect<Equal<IsPalindrome<'abc'>, false>>,
   Expect<Equal<IsPalindrome<'b'>, true>>,
   Expect<Equal<IsPalindrome<'abca'>, false>>,
+  Expect<Equal<IsPalindrome<'abba'>, true>>,
   Expect<Equal<IsPalindrome<'abcba'>, true>>,
   Expect<Equal<IsPalindrome<121>, true>>,
+  Expect<Equal<IsPalindrome<2332>, true>>,
   Expect<Equal<IsPalindrome<19260817>, false>>,
 ]
