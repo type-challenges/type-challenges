3637f1b877e11de8c848c3a85ce944e61876a0c1
bowen <54492610+jiaowoxiaobala@users.noreply.github.com>
2023-06-05 02:36:03 +0000

    feat(question): add #27958 - CheckRepeatedTuple

---
+++ questions/27958-medium-checkrepeatedtuple/test-cases.ts
@@ -0,0 +1,11 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CheckRepeatedTuple<[number, number, string, boolean]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[number, string]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 3]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 1]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<string[]>, false>>,
+]
