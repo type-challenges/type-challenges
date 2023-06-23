582d22deb86477dd70ecdb2497cb8c4f9f846ee2
Davydov_Konstantin <k.davydov.dev@gmail.com>
2023-06-23 16:10:02 +0400

    00268-easy-if

---
+++ solutions/00268-easy-if.ts
@@ -0,0 +1,30 @@
+/*
+  268 - If
+  -------
+  by Pavel Glushkov (@pashutk) #easy #utils
+
+  ### Question
+
+  Implement the util type `If<C, T, F>` which accepts condition `C`, a truthy value `T`, and a falsy value `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
+
+  For example:
+
+  ```ts
+  type A = If<true, 'a', 'b'>  // expected to be 'a'
+  type B = If<false, 'a', 'b'> // expected to be 'b'
+  ```
+
+  > View on GitHub: https://tsch.js.org/268
+*/
+
+import { Equal, Expect } from "../utils"
+
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
