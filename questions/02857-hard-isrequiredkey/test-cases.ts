6476243a2615f9057ffccca93ed84ff9d39ff4c7
bowen <54492610+jiaowoxiaobala@users.noreply.github.com>
2023-06-30 01:43:49 +0800

    test: add more test cases to 2857-IsRequiredKey (#28479)

--- questions/02857-hard-isrequiredkey/test-cases.ts
+++ questions/02857-hard-isrequiredkey/test-cases.ts
@@ -2,6 +2,8 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'a'>, true>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: string }, 'a'>, true>>,
   Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b'>, false>>,
   Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b' | 'a'>, false>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: undefined }, 'b' | 'a'>, true>>,
 ]
