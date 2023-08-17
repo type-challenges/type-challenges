70467156bcd6ad5e1cd1c9e628108b29304c9771
a_flappy_fish <19502359+JokerDr@users.noreply.github.com>
2023-08-17 07:52:57 +0000

    feat(question): add #29572 - ObjectValues

---
+++ questions/29572-hard-objectvalues/test-cases.ts
@@ -0,0 +1,15 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+interface TestCase1 {
+    a: number;
+    b: string;
+    c: 'this is c';
+    d: () => void;
+}
+
+type cases = [
+  Expect<Equal<ObjectValues<TestCase1> , [number, string,'this is c', () => void]>>,
+  Expect<Equal<ObjectValues<TestCase1,'a'|'b'|'d'> , [number, string, () => void]>>,
+  Expect<Equal<ObjectValues<TestCase1,"d"|"a"> ,[() => void, number]>>,// Add a little more difficulty (optional)
+]
