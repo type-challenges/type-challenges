8f9b1c84fd45b49406bc42c92fc553d7af64ad13
王金平 <32758131+NameWjp@users.noreply.github.com>
2023-09-01 22:42:02 +0800

    feat(question): add empty test branch (#29851)

--- questions/13580-hard-replace-union/test-cases.ts
+++ questions/13580-hard-replace-union/test-cases.ts
@@ -5,6 +5,9 @@ type cases = [
   // string -> null
   Expect<Equal<UnionReplace<number | string, [[string, null]]>, number | null>>,
 
+  // string -> null
+  Expect<Equal<UnionReplace<number | string, [[string, null], [Date, Function]]>, number | null>>,
+  
   // Date -> string; Function -> undefined
   Expect<Equal<UnionReplace<Function | Date | object, [[Date, string], [Function, undefined]]>, undefined | string | object>>,
 ]
