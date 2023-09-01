aa5bd12942955adf954d330f5bc741d9e3d3f0a2
bowen <54492610+jiaowoxiaobala@users.noreply.github.com>
2023-09-01 22:42:41 +0800

    test: add more test cases to 02822-split (#29842)

--- questions/02822-hard-split/test-cases.ts
+++ questions/02822-hard-split/test-cases.ts
@@ -1,6 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
+  Expect<Equal<Split<'Hi! How are you?'>, ['Hi! How are you?']>>,
   Expect<Equal<Split<'Hi! How are you?', 'z'>, ['Hi! How are you?']>>,
   Expect<Equal<Split<'Hi! How are you?', ' '>, ['Hi!', 'How', 'are', 'you?']>>,
   Expect<Equal<Split<'Hi! How are you?', ''>, ['H', 'i', '!', ' ', 'H', 'o', 'w', ' ', 'a', 'r', 'e', ' ', 'y', 'o', 'u', '?']>>,
@@ -8,5 +9,6 @@ type cases = [
   Expect<Equal<Split<'The sine in cosine', 'in'>, ['The s', 'e ', ' cos', 'e']>>,
   Expect<Equal<Split<'Never say never, forever and ever.', 'ver'>, ['Ne', ' say ne', ', fore', ' and e', '.']>>,
   Expect<Equal<Split<'', 'z'>, ['']>>,
+  Expect<Equal<Split<''>, ['']>>,
   Expect<Equal<Split<string, 'whatever'>, string[]>>,
 ]
