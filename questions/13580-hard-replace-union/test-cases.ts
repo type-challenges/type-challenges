65787bcd5584ed5b35fb83b2a203cd667b9c94cb
jiangshanmeta <jiangshanmeta@users.noreply.github.com>
2023-09-03 13:17:11 +0000

    chore: update index

--- questions/13580-hard-replace-union/test-cases.ts
+++ questions/13580-hard-replace-union/test-cases.ts
@@ -7,7 +7,7 @@ type cases = [
 
   // string -> null
   Expect<Equal<UnionReplace<number | string, [[string, null], [Date, Function]]>, number | null>>,
-  
+
   // Date -> string; Function -> undefined
   Expect<Equal<UnionReplace<Function | Date | object, [[Date, string], [Function, undefined]]>, undefined | string | object>>,
 ]
--- questions/29785-medium-deep-omit/test-cases.ts
+++ questions/29785-medium-deep-omit/test-cases.ts
@@ -2,7 +2,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type obj = {
   person: {
-    name: string;
+    name: string
     age: {
       value: number
     }
@@ -10,8 +10,8 @@ type obj = {
 }
 
 type cases = [
-  Expect<Equal<DeepOmit<obj, 'person'> , {}>>,
-  Expect<Equal<DeepOmit<obj, 'person.name'> , { person: { age: { value: number } } }>>,
-  Expect<Equal<DeepOmit<obj, 'name'> , obj>>,
-  Expect<Equal<DeepOmit<obj, 'person.age.value'> , { person: { name: string; age: {} } }>>,
+  Expect<Equal<DeepOmit<obj, 'person'>, {}>>,
+  Expect<Equal<DeepOmit<obj, 'person.name'>, { person: { age: { value: number } } }>>,
+  Expect<Equal<DeepOmit<obj, 'name'>, obj>>,
+  Expect<Equal<DeepOmit<obj, 'person.age.value'>, { person: { name: string; age: {} } }>>,
 ]
