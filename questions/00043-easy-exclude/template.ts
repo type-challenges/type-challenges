9af456ab520e2eb04a3493fbad386e24488b8931
shemil <shemilkaweesha@gmail.com>
2023-11-25 13:57:44 +0530

    Easy exclude answers

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,1 @@
-type MyExclude<T, U> = any
+type MyExclude<TType, TExclude> = TType extends TExclude ? never : TType
--- questions/00043-easy-exclude/test-cases.ts
+++ questions/00043-easy-exclude/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type sample = MyExclude<'a' | 'b' | 'c', 'a'>
+type example = 'a' | 'b' | 'c'
+
 type cases = [
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
