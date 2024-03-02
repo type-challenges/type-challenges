9bae5a3e540029ad523e46486829654aeb70ef62
antfu <antfu@users.noreply.github.com>
2024-03-02 00:03:45 +0000

    chore: update index

--- questions/32427-hard-unbox/test-cases.ts
+++ questions/32427-hard-unbox/test-cases.ts
@@ -1,13 +1,12 @@
 import type { Equal, Expect } from '@type-challenges/utils'
-import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
   // Base cases
   Expect<Equal<Unbox<number>, number>>,
-  Expect<Equal<Unbox<()=>number>, number>>,
-  Expect<Equal<Unbox<()=>number|string>, number|string>>,
+  Expect<Equal<Unbox<() => number>, number>>,
+  Expect<Equal<Unbox<() => number | string>, number | string>>,
   Expect<Equal<Unbox<number[]>, number>>,
-  Expect<Equal<Unbox<(number | string)[]>, number|string>>,
+  Expect<Equal<Unbox<(number | string)[]>, number | string>>,
   Expect<Equal<Unbox<[number]>, number>>,
   Expect<Equal<Unbox<Promise<number>>, number>>,
 
@@ -15,12 +14,12 @@ type cases = [
   Expect<Equal<Unbox<() => Promise<() => Array<Promise<boolean>>>>, boolean>>,
 
   // Bonus: Recusion levels
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 0>, number>>,
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 1>, ()=>()=>()=>number>>,
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 2>, ()=>()=>number>>,
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 3>, ()=>number>>,
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 4>, number>>,
-  Expect<Equal<Unbox<()=>()=>()=>()=>number, 5>, number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 0>, number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 1>, () => () => () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 2>, () => () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 3>, () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 4>, number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 5>, number>>,
   Expect<Equal<Unbox<number[][][][], 0>, number>>,
   Expect<Equal<Unbox<number[][][][], 1>, number[][][]>>,
   Expect<Equal<Unbox<number[][][][], 2>, number[][]>>,
