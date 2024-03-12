f43ac9a9c7041275a4fe19b985a773f7646f1551
OhJungJin <OhJungJin@users.noreply.github.com>
2024-03-12 01:06:32 +0000

    chore: update index

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,3 +1,3 @@
 type MyPick<T, K extends keyof T> = {
   [P in K]: T[P]
-};
+}
--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,1 @@
-type First<T extends any[]> = T extends [] ? never : T[0];
+type First<T extends any[]> = T extends [] ? never : T[0]
