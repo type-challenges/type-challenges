24c8d8ca8daff71f8f2af62f1558947a6ce10ab5
Kyungsu Kang <55487286+kakasoo@users.noreply.github.com>
2024-03-23 14:54:43 +0000

    feat(question): add #32719 - DeepPick

---
+++ questions/32719-medium-deeppick/README.md
@@ -0,0 +1,2 @@
+> Implement a type `DeepPick` Like Utility types [`Pick`](https://www.typescriptlang.org/docs/handbook/utility-types.html#picktype-keys), A type takes two arguments.
+The second generic parameter should be inferred as a literal string to help the developer write it.
---
+++ questions/32719-medium-deeppick/test-cases.ts
@@ -0,0 +1,24 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+
+type User = {
+  name: string;
+  age: number;
+  pets: {
+    cat: { nickname: string; age: number };
+    dog: { nickname: string; age: number };
+  };
+};
+
+type cases = [
+  Expect<Equal<DeepPick<User, 'pets.cat.nickname'>, { pets: { cat: { nickname: string } } }>>,
+  Expect<Equal<DeepPick<User, 'pets.cat'>, { pets: { cat: { nickname: string; age: number } } }>>,
+  Expect<Equal<DeepPick<User, 'pets.cat.age' | 'pets.cat.nickname'>, { pets: { cat: { nickname: string; age: number } } }>>,
+  Expect<Equal<DeepPick<User, 'age'>, { age: number }>>
+]
+
+type errors = [
+// @ts-expect-error: '"NON_EXISTENT_KEY"' is not assignable to type '"pets.cat.nickname" | "name" | "age" | "pets" | "pets.cat" | "pets.dog" | "pets.cat.age" | "pets.dog.age" | "pets.dog.nickname"'
+  Equal<DeepPick<User, 'NON_EXISTENT_KEY'>>
+]
