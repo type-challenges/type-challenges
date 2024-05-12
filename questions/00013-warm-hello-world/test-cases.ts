f1b453ab06da23f8c2011c43858ad5bd0a94bc88
otyamura <suzuki08suzuki@gmail.com>
2024-05-13 00:16:22 +0900

    :+1:  warm-hello-world

--- questions/00013-warm-hello-world/test-cases.ts
+++ questions/00013-warm-hello-world/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect, NotAny } from '@type-challenges/utils'
 
+type HelloWorld = string;
+
 type cases = [
   Expect<NotAny<HelloWorld>>,
   Expect<Equal<HelloWorld, string>>,
