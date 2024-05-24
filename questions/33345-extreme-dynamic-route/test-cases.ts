b99f3c51389dc9887edb10b6ba2ea07f0bfd1faa
antfu <antfu@users.noreply.github.com>
2024-05-24 00:04:56 +0000

    chore: update index

--- questions/33345-extreme-dynamic-route/test-cases.ts
+++ questions/33345-extreme-dynamic-route/test-cases.ts
@@ -1,5 +1,4 @@
 import type { Equal, Expect } from '@type-challenges/utils'
-import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<DynamicRoute<'/shop'>, {}>>,
@@ -7,54 +6,54 @@ type cases = [
   Expect<Equal<DynamicRoute<'/shop/[slug]'>, { slug: string }>>,
   Expect<Equal<DynamicRoute<'/shop/[slug]/'>, { slug: string }>>,
   Expect<
-    Equal<DynamicRoute<'/shop/[slug]/[foo]'>, { slug: string; foo: string }>
+    Equal<DynamicRoute<'/shop/[slug]/[foo]'>, { slug: string, foo: string }>
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[foo]'>,
-      { slug: string; foo: string }
+      { slug: string, foo: string }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[foo]'>,
-      { slug: string; foo: string }
+      { slug: string, foo: string }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[...foo]'>,
-      { slug: string; foo: string[] }
+      { slug: string, foo: string[] }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[[...foo]]'>,
-      { slug: string; foo?: string[] }
+      { slug: string, foo?: string[] }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[[...foo]]/[]'>,
-      { slug: string; foo?: string[] }
+      { slug: string, foo?: string[] }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[[...foo]]/[...]'>,
-      { slug: string; foo?: string[]; '...': string }
+      { slug: string, foo?: string[], '...': string }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[[...foo]]/[...]/[]index.html'>,
-      { slug: string; foo?: string[]; '...': string }
+      { slug: string, foo?: string[], '...': string }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'/shop/[slug]/stub/[[...foo]]/[...]/[...]index.html'>,
-      { slug: string; foo?: string[]; '...': string }
+      { slug: string, foo?: string[], '...': string }
     >
   >,
   Expect<Equal<DynamicRoute<'/[slug]/[[...foo]]/[...bar]'>, never>>,
@@ -63,13 +62,13 @@ type cases = [
   Expect<
     Equal<
       DynamicRoute<'[[...foo]]/stub/[...bar]'>,
-      { foo?: string[]; bar: string[] }
+      { foo?: string[], bar: string[] }
     >
   >,
   Expect<
     Equal<
       DynamicRoute<'[first]/[[...foo]]/stub/[...bar]/[last]'>,
-      { first: string; foo?: string[]; bar: string[]; last: string }
+      { first: string, foo?: string[], bar: string[], last: string }
     >
   >,
-];
+]
