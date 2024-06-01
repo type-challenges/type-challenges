9080836b5923f1716815e643342195d23b6a1617
ferllop <ferranllop@gmail.com>
2024-06-01 18:48:50 +0200

    feat: add test case for '-.3' in 05140-Trunc (#33403)
    Co-authored-by: Ferran Llop <contacto@ferranllop.com>

--- questions/05140-medium-trunc/test-cases.ts
+++ questions/05140-medium-trunc/test-cases.ts
@@ -8,6 +8,7 @@ type cases = [
   Expect<Equal<Trunc<-5.1>, '-5'>>,
   Expect<Equal<Trunc<'.3'>, '0'>>,
   Expect<Equal<Trunc<'1.234'>, '1'>>,
+  Expect<Equal<Trunc<'-.3'>, '-0'>>,
   Expect<Equal<Trunc<'-10.234'>, '-10'>>,
   Expect<Equal<Trunc<10>, '10'>>,
 ]
