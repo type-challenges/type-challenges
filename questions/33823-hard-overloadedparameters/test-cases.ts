ff75d010b98529c9a9de2202d84d584058e3a50b
Steven <25894545+teamchong@users.noreply.github.com>
2024-07-15 09:23:44 +0000

    feat(question): add #33823 - OverloadedParameters

---
+++ questions/33823-hard-overloadedparameters/test-cases.ts
@@ -0,0 +1,23 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+function func(fn: (args: string) => string): string
+
+function func(num: number, init: Set<string>): boolean
+
+function func(...args: unknown[]): string | boolean {
+  const [fnOrNum, init] = args as OverloadedParameters<typeof func>
+
+  if (typeof fnOrNum === 'function') {
+    return fnOrNum("foo")
+  } else {
+    return fnOrNum === init.size
+  }
+}
+
+func((s) => `Hello ${s}`)
+func(1, new Set<string>('Hi'))
+
+type cases = [
+  Expect<Equal<OverloadedParameters<typeof func>, [(args: string) => string, never] | [number, Set<string>]>>,
+]
