c52d163e508437b0a81f3bb500bec01f1a323f7d
MAXLZ <2251683416@qq.com>
2024-08-20 17:37:00 +0800

    test: add some test cases to 9898

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/test-cases.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/test-cases.ts
@@ -4,4 +4,6 @@ type cases = [
   Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
   Expect<Equal<FindEles<[2, 2, 3, 3, 6, 6, 6]>, []>>,
   Expect<Equal<FindEles<[1, 2, 3]>, [1, 2, 3]>>,
+  Expect<Equal<FindEles<[1, 2, number]>, [1, 2, number]>>,
+  Expect<Equal<FindEles<[1, 2, number, number]>, [1, 2]>>,
 ]
