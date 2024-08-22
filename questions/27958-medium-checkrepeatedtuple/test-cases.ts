a05088f3a78b7ce14e1f47cf02593e740828f4cc
drylint <80906525+drylint@users.noreply.github.com>
2024-08-22 10:22:39 +0800

    test(27958-medium-checkrepeatedtuple): add test cases (#34140)

--- questions/27958-medium-checkrepeatedtuple/test-cases.ts
+++ questions/27958-medium-checkrepeatedtuple/test-cases.ts
@@ -7,4 +7,6 @@ type cases = [
   Expect<Equal<CheckRepeatedTuple<[1, 2, 1]>, true>>,
   Expect<Equal<CheckRepeatedTuple<[]>, false>>,
   Expect<Equal<CheckRepeatedTuple<string[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[number, 1, string, '1', boolean, true, false, unknown, any]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[never, any, never]>, true>>,
 ]
