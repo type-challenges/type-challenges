dab501c4665ce420cc7b3391f0602f8b7e055ef6
Wankong <49053562+wan-kong@users.noreply.github.com>
2024-11-26 19:28:17 +0800

    case:remove zero case (#35128)
    Co-authored-by: zhubzh <zhubzh@motanni.com>

--- questions/02257-medium-minusone/test-cases.ts
+++ questions/02257-medium-minusone/test-cases.ts
@@ -6,6 +6,5 @@ type cases = [
   Expect<Equal<MinusOne<3>, 2>>,
   Expect<Equal<MinusOne<100>, 99>>,
   Expect<Equal<MinusOne<1101>, 1100>>,
-  Expect<Equal<MinusOne<0>, -1>>,
   Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
 ]
