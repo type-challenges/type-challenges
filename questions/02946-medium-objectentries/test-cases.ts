bbbdceb005fcb2cf5165a726a9f468f5cb60b923
Daniel Bates <danielalanbates@gmail.com>
2026-02-28 22:18:59 -0800

    fix: correct ObjectEntries test case for Partial<Model> (#38036)

--- questions/02946-medium-objectentries/test-cases.ts
+++ questions/02946-medium-objectentries/test-cases.ts
@@ -7,10 +7,11 @@ interface Model {
 }
 
 type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
+type PartialModelEntries = ['name', string | undefined] | ['age', number | undefined] | ['locations', string[] | null | undefined]
 
 type cases = [
   Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
-  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, PartialModelEntries>>,
   Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
   Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>,
   Expect<Equal<ObjectEntries<{ key: string | undefined }>, ['key', string | undefined]>>,
